7e728e0dd85d9b79551362a0753bcce1c3533363
Curtis Upshall <curtisupshall@gmail.com>
2019-03-14 14:22:43 -0700

    More axios attempts

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -3,7 +3,6 @@ import '../../assets/styles/main.scss'
 import * as React from 'react'
 import axios from 'axios'
 import { Sidebar } from '../Sidebar/Sidebar'
-import { Hello } from '../Hello'
 
 interface IProps {
 	compiler: string
@@ -11,21 +10,11 @@ interface IProps {
 }
 
 export default class App extends React.Component<IProps> {
-	componentDidMount() {
-		axios.get('http://localhost:8000/api/staff')
-			.then(res => {
-				const data = res.data
-				console.log(data)
-			})
-		console.log('axios did a thing...')
-	}
 	render() {
 		return ( 
 			<>
 				<div>
 					<Sidebar />
-					<Hello {...this.props}/>
-					<h1>Test</h1>
 				</div>
 			</>
 		)
--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react'
+
+import axios from 'axios'
+
 import IconButton from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
 
 export class Sidebar extends React.Component {
+	componentDidMount() {
+		axios.defaults.headers.post['Content-Type'] ='application/json';
+		axios.get('http://localhost:8000/api/staff', {
+			headers: {
+				'Access-Control-Allow-Origin': '*'
+			}
+		})
+			.then(res => {
+				const data = res.data
+				console.log(data)
+			})
+		console.log('axios did a thing2...')
+	}
+
 	render() {
+		console.log('sidebar rendered')
 		return (
-			<>
+			<>{/*
 				<div className='sidebar'>
 					<nav className='sidebar__nav'>
 						<IconButton><Icon>alarm</Icon></IconButton>
@@ -18,7 +36,7 @@ export class Sidebar extends React.Component {
 						<li className='sidebar__menu__item'>Students</li>
 					</ul>
 				</div>
-
+			*/}
 			</>
 		)
 	}

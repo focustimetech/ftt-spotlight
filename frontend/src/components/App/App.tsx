e25626d8ad1552e75b00e886f6400ca854af70dd
Curtis Upshall <curtisupshall@gmail.com>
2019-03-13 22:36:15 -0700

    axios implementation

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import '../../assets/styles/main.scss'
 
 import * as React from 'react'
+import axios from 'axios'
 import { Sidebar } from '../Sidebar/Sidebar'
 import { Hello } from '../Hello'
 
@@ -10,11 +11,23 @@ interface IProps {
 }
 
 export default class App extends React.Component<IProps> {
+	componentDidMount() {
+		axios.get('http://localhost:8000/api/staff')
+			.then(res => {
+				const data = res.data
+				console.log(data)
+			})
+		console.log('axios did a thing...')
+	}
 	render() {
-		return <div>
-			<Sidebar />
-			<Hello {...this.props}/>
-		</div>
-		
+		return ( 
+			<>
+				<div>
+					<Sidebar />
+					<Hello {...this.props}/>
+					<h1>Test</h1>
+				</div>
+			</>
+		)
 	}
 }

54e7ac5aa26ad018176aa6e7cdcf9e9041a66a7e
Curtis Upshall <curtisupshall@gmail.com>
2019-03-24 11:36:06 -0700

    User + password login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -16,6 +16,20 @@ interface IState {
 	password: string
 }
 
+const selectBackground = () => {
+	const imageList: string[] = [
+		'ali-yahya-782497-unsplash.jpg',
+		'brooke-cagle-609873-unsplash.jpg',
+		'helloquence-61189-unsplash.jpg',
+		'john-schnobrich-520023-unsplash.jpg',
+		'mimi-thian-737597-unsplash.jpg',
+		'mimi-thian-737711-unsplash.jpg',
+		'priscilla-du-preez-293218-unsplash.jpg'
+	]
+	const arrayIndex: number = Math.floor(Math.random() * imageList.length)
+	return `url('src/assets/images/splash/${imageList[arrayIndex]}')`
+}
+
 export class Login extends React.Component<{}, IState> {
 	state = {
 		step: 'user' as LoginStep,
@@ -28,25 +42,15 @@ export class Login extends React.Component<{}, IState> {
 		this.setState({ [event.target.name]: event.target.value } as IState)
 	}
 	
-	handleContinue = () => {
-		console.log('handleContinue()')
-		this.setState({ step: 'password' })
-	}
-
 	handleLogin = () => {
 		console.log('handleLogin()')
-		this.setState({ step: 'user' })
-	}
-
-	cancelUser = () => {
-		console.log('cancelUser()')
 		this.setState({ step: 'user' })
 	}
 
 	render() {
 		return (
 			<div className='login'>
-				<div className='login__about'>
+				<div className='login__about' style={{backgroundImage: selectBackground()}}>
 					<a href='https://focustime.ca' className='logo_container'>
 						<h1>Spotlight</h1>
 					</a>
@@ -57,46 +61,34 @@ export class Login extends React.Component<{}, IState> {
 						<a href='https://focustime.ca' className='subtitle_link'>Start using powerful tools that let your self directed study blocks succeed.</a>
 						<form className='login_form'>
 							<h2>Sign in</h2>
-							{this.state.step === 'user' && 
-								<>
-									<TextField
-										name='user'
-										type='text'
-										label='Email or Student Number'
-										value={this.state.user}
-										onChange={this.handleChange}
-										margin='normal'
-										variant='filled'
-										autoFocus={true}
-										fullWidth={true}
-									/>
-									<div className='button_container'>
-										<Button onClick={() => this.handleContinue()} color='primary' variant='contained'>Continue</Button>
-									</div>
-								</>}
-							{this.state.step === 'password' &&
-								<>
-									<TextField
-										name='password'
-										type='password'
-										label='Password'
-										value={this.state.password}
-										onChange={this.handleChange}
-										margin='normal'
-										variant='filled'
-										autoFocus={true}
-										fullWidth={true}
-									/>
-									<div className='button_container'>
-										<Button onClick={() => this.cancelUser()} variant='text'>Cancel</Button>
-										<Button onClick={() => this.handleLogin()} color='primary' variant='contained'>Sign In</Button>
-									</div>
-								</>
-							}
+							<TextField
+								name='user'
+								type='text'
+								label='Email or Student Number'
+								value={this.state.user}
+								onChange={this.handleChange}
+								margin='normal'
+								variant='filled'
+								autoFocus={true}
+								fullWidth={true}
+							/>
+							<TextField
+								name='password'
+								type='password'
+								label='Password'
+								value={this.state.password}
+								onChange={this.handleChange}
+								margin='normal'
+								variant='filled'
+								autoFocus={true}
+								fullWidth={true}
+							/>
+							<div className='button_container'>
+								<Button onClick={() => this.handleLogin()} color='primary' variant='contained'>Sign In</Button>
+							</div>
 						</form>
 						<ul className='links_list'>
 							<a href='https://focustime.ca'><li>Help</li></a>
-							<a href='https://focustime.ca'><li>Legal</li></a>
 						</ul>
 					</div>
 				</div>

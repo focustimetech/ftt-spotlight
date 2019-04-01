7faa0bc0cf8b6024c6e715907b1c5854af41616a
Curtis Upshall <curtisupshall@gmail.com>
2019-04-01 09:54:01 -0700

    Refactor schedule using table style

--- frontend/src/components/Schedule.tsx
+++ frontend/src/components/Schedule.tsx
@@ -4,48 +4,46 @@ export class Schedule extends React.Component {
 	render() {
 		return (
 			<div className='schedule'>
-				<div className='schedule_column'>
-					<div className='schedule_column__label'>
+				<div className='schedule_row'>
+					<div className='label'>
 						<h5 className='day'>Mon</h5>
 						<h2 className='date'>25</h2>
 					</div>
-					<div className='schedule_column__events'>
-						<div className='schedule_event'>Assembly</div>
-					</div>
-					<div className='schedule_column__blocks'>
-						<div className='schedule_block --missed'>Hello world</div>
-						<div className='schedule_block --attended'>Hello world</div>
-						<div className='schedule_block --void'>Hello world</div>
-						<div className='schedule_block --attended'>Hello world</div>
-						<div className='schedule_block --attended'>Hello world</div>
-					</div>
-				</div>
-				<div className='schedule_column'>
-					<div className='schedule_column__label --today'>
+					<div className='label --today'>
 						<h5 className='day'>Tue</h5>
 						<h2 className='date'>26</h2>
 					</div>
-					<div className='schedule_column__events'>
-					</div>
-					<div className='schedule_column__blocks'>
-						<div className='schedule_block --missed'>Hello world</div>
-						<div className='schedule_block --attended'>Hello world</div>
-						<div className='schedule_block --pending --now'>Hello world</div>
-						<div className='schedule_block --appointed'>Hello world</div>
-					</div>
-				</div>
-				<div className='schedule_column'>
-					<div className='schedule_column__label'>
+					<div className='label'>
 						<h5 className='day'>Wed</h5>
 						<h2 className='date'>27</h2>
 					</div>
-					<div className='schedule_column__events'>
-						<div className='schedule_event'>Assembly</div>
+				</div>
+				<div className='schedule_row'>
+					<div className='schedule_events'>
+						<div className='event'>Assembly</div>
+						<div className='event'>Another Assembly :-o</div>
 					</div>
-					<div className='schedule_column__blocks'>
-						<div className='schedule_block --pending'>Hello world</div>
-						<div className='schedule_block --pending'>Hello world</div>
-						<div className='schedule_block --pending'>Hello world</div>
+					<div className='schedule_events'></div>
+					<div className='schedule_events'>
+						<div className='event'>Friday Breakfast</div>
+					</div>
+				</div>
+				<div className='schedule_row'>
+					<div className='schedule_blocks'>
+						<div className='block --missed'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
+						<div className='block --void'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
+					</div>
+					<div className='schedule_blocks'>
+						<div className='block --void'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
+					</div>
+					<div className='schedule_blocks'>
+						<div className='block --missed'>Hello world</div>
+						<div className='block --attended'>Hello world</div>
 					</div>
 				</div>
 			</div>

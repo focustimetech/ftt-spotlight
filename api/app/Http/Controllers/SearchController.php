ec2ed93521ac2d2302d06726be544925104ff095
Curtis Upshall <curtisupshall@gmail.com>
2019-04-01 12:16:28 -0700

    Search results gathering

--- api/app/Http/Controllers/SearchController.php
+++ api/app/Http/Controllers/SearchController.php
@@ -11,13 +11,30 @@ class SearchController extends Controller
 {
     public function search(Request $request) {
         $query = '%'. $request->get('query'). '%';
-        // return $request;
-        $result = [];
-        //$staff = Staff::where('lastname', 'like', "%$query%");
+        $result = [
+            'students' => [],
+            'staff' => [],
+            'courses' => []
+        ];
+        $staff = Staff::where('lastname', 'like', "'$query'");
         $students = DB::table('students')
             ->whereRaw("first_name LIKE '$query' OR last_name LIKE '$query' 
             OR CONCAT(first_name, ' ', last_name) LIKE '$query'")->get();
-        //$courses = Courses::where('name', 'like', "%$query%");
-        return $students; //array_merge($students, $staff, $courses);
+        $courses = Course::where('name', 'like', "'$query'");
+        $allResults = [
+            'students' => $students,
+            'staff' => $staff,
+            'courses' => $courses
+        ];
+        // Pepper search results into array s.t. we always have 15 results or less
+        for ($index = 0; count($results) < 15; $index ++) {
+            foreach ($allResults as $key => $collection) {
+                if ($collection[$index]) {
+                    array_push($result[$key], $collection[$index]);
+                }
+                if (count($results) === 15) break;
+            }
+        }
+        return $result; //array_merge($students, $staff, $courses);
     }
 }

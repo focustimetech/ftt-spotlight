befde8027c884558e8dc791f0d574c84f2483229
Curtis Upshall <curtisupshall@gmail.com>
2019-04-24 16:22:03 -0700

    Filters from stateless component to class

--- frontend/src/components/Table/EnhancedTableFilter.tsx
+++ frontend/src/components/Table/EnhancedTableFilter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import {
 	Button,
+	Icon,
 	IconButton,
 	MenuItem,
 	Paper,
@@ -14,12 +15,9 @@ import {
 	ITableHeaderColumn,
 	ITableNumericFilterType,
 	ITableStringFilterType,
+	ITableStringFilter,
+	ITableNumericFilter,
 } from '../../types/table';
-
-interface FilterType {
-	string: StringFilterType[]
-	numeric: NumericFilterType[]
-}
 
 interface StringFilterRule {
 	label: string,
@@ -52,78 +50,119 @@ interface IProps {
 	handleFilterChange: (filters: ITableFilter[]) => void
 }
 
-export const EnhancedTableFilter = (props: IProps) => {
+interface IState {
+	filters: ITableFilter[]
+}
 
-	const onAddFilter = () => {
-		const newFilter: ITableFilter = {
-			...props.filters[props.filters.length - 1],
-			value: ''
+/**
+ * @TODO MSeparate the handleFilterChange into 3 methods.
+ */
+export class EnhancedTableFilter extends React.Component<IProps, IState> {
+	state: IState = {
+		filters: this.props.filters.length ? this.props.filters : [this.newFilter()]
+	}
+
+	private newFilter(): ITableFilter {
+		let filter: ITableFilter
+		if (this.props.filters.length) {
+			filter = {
+				...this.props.filters[this.props.filters.length - 1],
+				value: ''
+			}
+		} else {
+			if (this.props.columns[0].isNumeric) {
+				filter = {
+					id: this.props.columns[0].id,
+					type: 'numeric',
+					rule: numericFilterRules[0].value,
+					value: ''
+				}
+			} else {
+				filter = {
+					id: this.props.columns[0].id,
+					type: 'string',
+					rule: stringFilterRules[0].value,
+					value: ''
+				}
+			}
 		}
-		props.handleFilterChange(props.filters.concat(newFilter))
+		return filter
 	}
 
-	const onRemoveFilter = (index: number) => {
-		props.handleFilterChange(props.filters.filter((filter, idx) => {
-			return index !== idx
-		}))
+	onAddFilter = () => {
+		this.setState((state: IState) => {
+			return {filters: state.filters.concat(this.newFilter()) }
+		})
 	}
 
-	const handleFilterChange = (value: string, field: 'rule' | 'value' | 'id', index: number) => {
-		const filters: ITableFilter[] = props.filters
-		filters[index][field] = value
-		props.handleFilterChange(filters)
+	onRemoveFilter = (index: number) => {
+		this.setState((state: IState) => {
+			return { 
+				filters: state.filters.filter((filter, idx) => {
+					return index !== idx
+				})
+			}
+		})
 	}
 
-	const onRemoveAllFilters = () => {
-		props.handleFilterChange([])
+	onRemoveAllFilters = () => {
+		this.setState({ filters: [] })
 	}
 
-	return (
-		<Paper className='enhanced-table__filters'>
-			<h3>Filters</h3>
-			{props.filters.length ? (
-				<ul>
-					{props.filters.map((filter: ITableFilter, idx: number) => {
-						const filterRules: Array<StringFilterRule | NumericFilterRule> = filter.type === 'string' ? (
-							stringFilterRules
-						) : (
-							numericFilterRules
-						)
-						return (
-							<li key={idx}>
-								<Select
-									name='id'
-									value={filter.id}
-								>{props.columns.map((column: ITableHeaderColumn) => {
-									return (
-										<MenuItem value={column.id}>
-											{column.label}
-										</MenuItem>
-									)
-								})}
-								</Select>
-								<Select
-									name='rule'
-									value={filter.rule}
-								>	
-									{filterRules.map((filterRule) => (
-										<MenuItem value={filterRule.value}>
-											{filterRule.label}
-										</MenuItem>
-									))}
-								</Select>
-								<TextField
-									variant='standard'
-								/>
-							</li>
-						)
-					})}
-				</ul>
-			) : (
-				<p>No filters added.</p>
-			)}
-			<Button onClick={() => onAddFilter()}>Add Filter</Button>
-		</Paper>
-	)
+	render() {
+		return (
+			<Paper className='enhanced-table__filters'>
+				<h3>Filters</h3>
+				{this.state.filters.length ? (
+					<ul>
+						{this.state.filters.map((filter: ITableFilter, idx: number) => {
+							const filterRules: Array<StringFilterRule | NumericFilterRule> = filter.type === 'string' ? (
+								stringFilterRules
+							) : (
+								numericFilterRules
+							)
+							return (
+								<li key={idx}>
+									<Select
+										name='id'
+										value={filter.id}
+										onChange={(event: any) => {handleFilterChange(event.target.value, 'id', idx)}}
+									>
+										{this.props.columns.map((column: ITableHeaderColumn) => {
+											return (
+												<MenuItem value={column.id}>
+													{column.label}
+												</MenuItem>
+											)
+										})}
+									</Select>
+									<Select
+										name='rule'
+										value={filter.rule}
+										onChange={(event: any) => {handleFilterChange(event.target.value, 'rule', idx)}}
+									>	
+										{filterRules.map((filterRule) => (
+											<MenuItem value={filterRule.value}>
+												{filterRule.label}
+											</MenuItem>
+										))}
+									</Select>
+									<TextField
+										variant='standard'
+										onChange={(event: any) => {handleFilterChange(event.target.value, 'value', idx)}}
+										value={filter.value}
+									/>
+									<IconButton onClick={() => this.onRemoveFilter(idx)}><Icon>close</Icon></IconButton>
+								</li>
+							)
+						})}
+					</ul>
+				) : (
+					<p>No filters added.</p>
+				)}
+				<Button onClick={() => this.onAddFilter()}>Add Filter</Button>
+			</Paper>
+		)
+	}
 
 }

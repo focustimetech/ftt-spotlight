1345c820c4a77007d5576b9991448219469f4f7d
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-10 12:00:02 -0700

    Begin Redux for clusters

--- frontend/src/actions/types.ts
+++ frontend/src/actions/types.ts
@@ -1,2 +1,11 @@
+// Students
 export const FETCH_STUDENTS = 'FETCH_STUDENTS'
 export const NEW_STUDENT = 'NEW_STUDENT'
+export const DELETE_STUDENT = 'DELETE_STUDENT'
+export const UPDATE_STUDENT = 'UPDATE_STUDENT'
+
+// Clusters
+export const FETCH_CLUSTERS = 'FETCH_CLUSTERS'
+export const NEW_CLUSTER = 'NEW_CLUSTER'
+export const DELETE_CLUSTER = 'DELETE_CLUSTER'
+export const UPDATE_CLUSTER = 'UPDATE_CLUSTER'

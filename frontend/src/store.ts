48bd864b1686b1fbefddbec3981329246405be81
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-12 15:01:01 -0700

    Clean up store.ts

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -5,15 +5,7 @@ import { rootReducer } from './reducers'
 const initialState = {}
 const middleware = [thunk]
 
-interface IWindow extends Window {
-    __REDUX_DEVTOOLS__EXTENSION__: any
-
-}
-
-// const enhancer: any = (window as Window)['devToolsExtension'] ? (window as Window)['devToolsExtension']()(createStore) : createStore
-
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
 
 export const store = createStore(
     rootReducer,

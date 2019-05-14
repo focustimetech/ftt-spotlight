3f8e6ec1c1da2c3549e36774c417a1ca699aa848
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-14 16:33:13 -0700

    Verbose search results

--- api/app/Http/Controllers/SearchController.php
+++ api/app/Http/Controllers/SearchController.php
@@ -5,54 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Staff;
 use App\Course;
+use App\Cluster;
 use DB;
 
 class SearchController extends Controller
 {
     public function search(Request $request) {
         $query = '%'. $request->get('query'). '%';
-        $limit = 6;
+        $limit = 5;
         $result = [
-            [
-                'label' => 'Students',
-                'values' => DB::table('students')
-                    ->whereRaw("first_name LIKE '$query' OR last_name LIKE '$query' OR CONCAT(first_name, ' ', last_name) LIKE '$query'")
-                    ->limit($limit)
-                    ->get()
-                    ->map(function ($student) {
-                        return [
-                            'value' => $student->first_name. ' '. $student->last_name,
-                            'type' => 'student',
-                            'url' => 'students/'. $student->id
-                        ];
-                    })
-            ],
-            [
-                'label' => 'Staff',
-                'values' => Staff::where('last_name', 'like', $query)
-                    ->limit($limit)
-                    ->get()
-                    ->map(function ($staff) {
-                        return [
-                            'value' => $staff->title. ' '. $staff->first_name. ' '. $staff->last_name,
-                            'type' => 'staff',
-                            'url' => 'staff/'. $staff->id
-                        ];
-                    })
-            ],
-            [
-                'label' => 'Courses',
-                'values' => Course::where('name', 'like', $query)
-                    ->limit($limit)
-                    ->get()
-                    ->map(function ($course) {
-                        return [
-                            'value' => $course->name,
-                            'type' => 'course',
-                            'url' => 'courses/'. $course->short_name // TODO: Add restrictions on short_name. (must be like slug)
-                        ];
-                    })
-            ]
+            'students' => DB::table('students')
+                ->whereRaw("first_name LIKE '$query' OR last_name LIKE '$query' OR CONCAT(first_name, ' ', last_name) LIKE '$query'")
+                ->limit($limit)
+                ->get(),
+
+            'staff' => Staff::where('last_name', 'like', $query)
+                ->limit($limit)
+                ->get(),
+
+            'courses' => Course::where('name', 'like', $query)
+                ->limit($limit)
+                ->get(),
+
+            'clusters' => Cluster::where('name', 'like', $query)
+                ->limit($limit)
+                ->get()
         ];
 
         return [

41e9aa92beddcd513dd9d3038a748feee51b7eda
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-15 09:15:00 -0700

    Create Appointments and Attendance

---
+++ frontend/src/components/Attendance.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+interface IProps {}
+
+/**
+ * Later this state-less component will be made into a class to use with Redux.
+ */
+export const Attendance = (props: IProps) => {
+    return (
+        <div>Attendance</div>
+    )
+}

1ae60103d63850f30395da9e140bf9cddbc3e3b5
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-19 18:52:30 -0700

    Remove withTimestamp

--- api/app/Block.php
+++ api/app/Block.php
@@ -9,7 +9,7 @@ class Block extends Model
     public function courses()
     {
         return $this->belongsToMany('App\Course', 'block_course', 'block_id', 'course_id')
-            ->withPivot('staff_id')
-            ->withTimestamps();
+            ->withPivot('staff_id');
+            // ->withTimestamps();
     }
 }

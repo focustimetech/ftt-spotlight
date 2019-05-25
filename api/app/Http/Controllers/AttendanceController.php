8a3d51d9e6f3014145afbf55a621c08a10d67ecf
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-24 21:53:07 -0700

    AttendanceController pseudocode

--- api/app/Http/Controllers/AttendanceController.php
+++ api/app/Http/Controllers/AttendanceController.php
@@ -23,26 +23,51 @@ class AttendanceController extends Controller
             $enrolled_at = $course->enrolled_at;
             $stop_at = $course->dropped_at ?? $now;
             $block_schedule = $student->blockSchedule()->groupBy('day_of_week');
-            $schedule_length = count($block_schedule);
-            $i = 0;
 
             // First, get the very first block in which attendance is recorded
             $start_time = date('H:i:s', $enrolled_at);
             $start_day = date('w', $enrolled_at);
-            while ($start_day !== $block_schedule[$i]->day_of_week && $start_time >= $block_schedule[$i]->end) {
+            $i = 0;
+            $k = 0;
+            while ($start_day !== $block_schedule[$i]->day_of_week) {
                 $i ++;
-                if ($i === $schedule_length) {
+                if ($i === count($block_schedule)) {
+                    break;
+                }
+            }
+            while ($start_time >= $block_schedule[$i][$k]->end) {
+                $k ++;
+                if ($k === count($block_schedule[$i])) {
                     break;
                 }
             }
 
             // Iterate over each block;
-            $date = date('Y-m-d', $enrolled_at);
-            $time = $start_time;
-            for ($time = strtotime("$date $time"); $time <= $stop_at; $i ++) {
-                $block = $block_schedule[$i % $schedule_length];
-                
-                $time = strtotime("$date $time");
+            $week = start_of_week($enrolled_at);
+            for (;;) {
+                for (; $i < count($block_schedule); $i ++) {
+                    $date = $week + $block_schedule[$i][0]->day_of_week;
+                    $attendance[$date] = ['attended' => 0, 'missed' => 0];
+                    for (; $k < count($block_schedule[$i]); $k ++) {
+                        $block = $block_schedule[$i][$k];
+                        $block_id = $block->id;
+                        $time = $block->start;
+
+                        if (date($date + $time) > $stop_at) {
+                            return $attendance;
+                        } else {
+                            $ledger_entry = null; // = App\LedgerEntry::find(...);
+                            if ($ledger_entry == null) {
+                                $attendance[$date]['missed'] ++;
+                            } else {
+                                $attendance[$date]['attended'] ++;
+                            }
+                        }
+                    }
+                    $k = 0;
+                }
+                $i = 0;
+                $week ++;
             }
         });
     }

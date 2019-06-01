c243b6d933c2723f68d8c590ee5900cbd2ce2e93
Curtis Upshall <curtisupshall@uvic.ca>
2019-06-01 09:24:42 -0700

    Begin modelling attendance

--- frontend/src/components/Attendance.tsx
+++ frontend/src/components/Attendance.tsx
@@ -10,18 +10,72 @@ import {
     IconButton
 } from '@material-ui/core'
 
-export class Attendance extends React.Component {
+const data = [
+    {
+        date: 'Monday, May 24',
+        attended: 4,
+        missed: 1
+    },
+    {
+        date: 'Tuesday, May 25',
+        attended: 6,
+        missed: 0
+    },
+    {
+        date: 'Wednesday, May 26',
+        attended: 6,
+        missed: 0
+    },
+    {
+        date: 'Thursday, May 27',
+        attended: 3,
+        missed: 3
+    },
+    {
+        date: 'Friday, May 28',
+        attended: 5,
+        missed: 0
+    }
+]
+
+interface IState {
+    expanded: number
+    loading: boolean
+}
+
+export class Attendance extends React.Component<{}, IState> {
+    state: IState = {
+        expanded: -1,
+        loading: false
+    }
 
     componentDidMount() {
         
     }
     
+    onExpandedChange = (panel: number) => {
+        this.setState((state: IState) => ({ expanded: state.expanded === panel ? -1 : panel }))
+    }
+
     render() {
         return (
             <>
                 <div>Attendance</div>
                 <div className='expansion-panel'>
+                    {data && data.length ? (
+                        data.map((record: any, index: number) => (
+                            <ExpansionPanel key={index} expanded={this.state.expanded === index} onClick={() => this.onExpandedChange(index)}>
+                                <ExpansionPanelSummary expandIcon={<Icon>expand_more</Icon>}>
+                                    <h3 className='expansion-panel__heading'>{`${record.attended}/${record.attended + record.missed}`}</h3>
+                                    <p className='expansion-panel__subheading'>{record.date}</p>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails className='expansion-panel__details'><p>Info goes here.</p></ExpansionPanelDetails>
 
+                            </ExpansionPanel>
+                        ))
+                    ) : (
+                        <p>No attendance data available.</p>
+                    )}
                 </div>
             </>
         )

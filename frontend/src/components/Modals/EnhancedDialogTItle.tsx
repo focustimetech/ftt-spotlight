67e00f25528cd37f0f45c74307d9a40e34aa649e
Curtis Upshall <curtisupshall@uvic.ca>
2019-06-13 11:54:38 -0700

    Add comments + props to EnhancedDialogTitle

--- frontend/src/components/Modals/EnhancedDialogTItle.tsx
+++ frontend/src/components/Modals/EnhancedDialogTItle.tsx
@@ -8,13 +8,23 @@ import {
 
 interface IProps {
 	children?: any
+	className?: string
 	id?: string
 	title?: string
 	onClose?: () => void
 }
+
+/**
+ * An enhanced DialogTitle for material-ui Dialogs.
+ * @param children The contents of the DialogTitle (optional)
+ * @param className className that gets passed to the container div (optional)
+ * @param id id property that gets passed to the container div (optional) 
+ * @param title The title for the form (optional)
+ * @param onClose Optional callback, including which renders a close icon button
+ */
 export const EnhancedDialogTitle = (props: IProps) => {
 	return (
-		<div className={classNames('dialog-title', {'--close-button': props.onClose})} id={props.id}>
+		<div className={classNames('dialog-title', {'--close-button': props.onClose}, {[props.className]: new Boolean(props.className)})} id={props.id}>
 			<div>
 				{props.title && (
 					<h3>{props.title}</h3>

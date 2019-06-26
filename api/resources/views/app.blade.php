28597f1b57c44baa46b173aef21da9762c8c2143
Curtis Upshall <curtisupshall@uvic.ca>
2019-06-26 12:50:31 -0700

    Create web endpoint

---
+++ api/resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Spotlight - Smart Attendance</title>
+    </head>
+    <body>
+        <div id="app-root"></div>
+
+        <!-- Dependencies -->
+        <script src="{{ asset('/frontend/node_modules/react/umd/react.development.js') }}"></script>
+        <script src="{{ asset('/frontend/node_modules/react-dom/umd/react-dom.development.js') }}"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+
+        <!-- Main -->
+        <script src="{{ asset('/frontend/dist/bundle.js') }}"></script>
+    </body>
+</html>

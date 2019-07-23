166b76cc97d01a68fc1fe52a27e84363dc74b11d
Curtis Upshall <curtisupshall@uvic.ca>
2019-07-23 12:22:01 -0700

    Gather block information

--- api/app/Http/Controllers/AttendanceController.php
+++ api/app/Http/Controllers/AttendanceController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Block;
+use App\BlockSchedule;
 use App\Student;
 
 class AttendanceController extends Controller
@@ -10,68 +12,68 @@ class AttendanceController extends Controller
     /**
      * Get total attendance for a given student
      */
-    public function attendance($student_id)
+    public function attendance($student_id, $start_datetime = null)
     {
+        if ($start_datetime === null) {
+            $start_time = time();
+        } else {
+            $start_time = strtotime($start_datetime);
+        }
         $student = Student::findOrFail($student_id);
-        $blocks = $student->courses()->get()->map(function($course) {
-            $block = $course->getBlock();
-            return [
-                'block_id'
+        $student_blocks = []; // Blocks belonging to student
+        $settings_start_time = strtotime(app('settings')['start_datetime']);
+        $settings_end_time = strtotime(app('settings')['end_datetime']);
+
+        // First add course blocks
+        $courses = $student->courses()->get(); // Get student's enrollment
+        $courses->each(function($course) use (&$student_blocks) {
+            $blocks = $course->blocks()->get();
+            $blocks->each(function($block) use (&$student_blocks, $course) {
+                $student_blocks[$block->id] = [
+                    'label' => $block->label,
+                    'course_id' => $course->id,
+                    'enrolled_at' => $course->enrollment->enrolled_at,
+                    'dropped_at' => $course->enrollment->dropped_at,
+                    'flex' => $block->flex,
+                    'total' => 0, // Total number of occurances
+                    'attended' => 0 // Total number of ledger entries for the block
+                ];
+            });
+        });
+
+        // Second, add flex blocks
+        $flex_blocks = Block::flexBlocks();
+        $flex_blocks->each(function($flex_block) use (&$student_blocks) {
+            $student_blocks[$flex_block->id] = [
+                'label' => $flex_block->label,
+                'flex' => true,
+                'total' => 0,
+                'attended' => 0
             ];
         });
 
-        $block_schedule = $student->getBlockSchedule()->groupBy('day_of_week')->values()->toArray();
-        $attendance = [];
-
-        $courses->each(function($course) use (&$attendance, $student, $block_schedule) {
-            $enrolled_at = strtotime($course->enrollment->enrolled_at);
-            $stop_at = $course->enrollment->dropped_at ? strtotime($course->enrollment->dropped_at) : time();
-
-            // First, get the very first block in which attendance is recorded
-            $start_time = date('H:i:s', $enrolled_at);
-            $start_day = date('w', $enrolled_at) + 1;
-            for ($i = 0; $i < count($block_schedule) - 1 && $start_day !== $block_schedule[$i][0]['day_of_week']; $i ++);
-            for ($k = 0; $k < count($block_schedule[$i]) - 1 && $start_time < $block_schedule[$i][$k]['end']; $k ++);
-
-            // Iterate over each block;
-            $day_diff = date('w', $enrolled_at);
-            $week = strtotime("-$day_diff days", strtotime(date('Y-m-d', $enrolled_at)));
-            for (;;) {
-                for (; $i < count($block_schedule); $i ++) {
-                    $day_diff = $block_schedule[$i][0]['day_of_week'] - 1;
-                    $timestmp = strtotime("+$day_diff days", $week);
-                    $date = date('Y-m-d', $timestmp);
-                    // echo "Date: $date;";
-                    //return;
-                    $attendance["$date"] = ['attended' => 0, 'missed' => 0];
-                    // echo var_dump($attendance);
-                    // return;
-                    for (; $k < count($block_schedule[$i]); $k ++) {
-                        $block = $block_schedule[$i][$k];
-                        $block_id = $block['block_id'];
-                        $time = $block['start'];
-                        //echo "Date: $date, \$block_id: $block_id; ";
-
-                        if (strtotime("$date $time") > $stop_at) {
-                            // echo "strtotime('$date $time') > $stop_at";
-                            return;
-                        } else {
-                            $ledger_entry = $student->ledgerEntries()->where('date', $date)->where('block_id', $block_id)->first();
-                            if ($ledger_entry == null) {
-                                $attendance["$date"]['missed'] ++;
-                                // echo "Missed on $date;";
-                            } else {
-                                $attendance["$date"]['attended'] ++;
-                                // echo "Attended on $date;";
-                            }
-                        }
-                    }
-                    $k = 0;
-                }
-                $i = 0;
-                $week = strtotime('+1 week', $week);
-            }
+        // Get Block Schedules by key
+        $schedule_blocks = BlockSchedule::all()->mapToGroups(function($schedule_block) {
+            return [$schedule_block->block_id => $schedule_block];
         });
-        return $attendance;
+
+        // Calculate missed and attended counts for each block
+        foreach ($student_blocks as $student_block) {
+            if ($student_block['flex'] === true) {
+                $start = $start_time > $settings_start_time ? $start_time : $settings_start_time;
+                $end = $settings_end_time < time() ? $settings_end_time : time();
+            } else {
+                // Regular course block
+                $enrolled_time = strtotime($block->enrolled_at);
+
+                // Drop timestamp is possible null in the case that student is continuing class
+                $dropped_time = $block->dropped_at ? strtotime($block->dropped_at) : null;
+                $start = $start_time > $enrolled_time ? $start_time : $enrolled_time > $settings_start_time ? $enrolled_time : $settings_start_time;
+                $end = $dropped_at && $dropped_at < time() ? $dropped_at : time() < $settings_end_time ? time() : $settings_end_time;
+            }
+        }
+
+        
+        return $student_blocks;
     }
 }

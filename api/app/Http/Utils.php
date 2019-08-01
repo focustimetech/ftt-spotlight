1e1fec5283bbbdcc7f5012582c2442a54a2318af
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-01 00:01:41 -0700

    Approximate time helper function

--- api/app/Http/Utils.php
+++ api/app/Http/Utils.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace App\Http;
+
+use DateTime;
+
+/*
 const USER_COLORS = [
     'F44336',
     'E91E63',
@@ -21,6 +26,7 @@ const USER_COLORS = [
     '9E9E9E',
     '607D8B'
 ];
+*/
 
 /**
  * Class for useful utility functions.
@@ -42,5 +48,38 @@ class Utils {
      */
     public static function topicColor($index = -1) {
         return self::userColor($index);
+    }
+
+    /**
+     * Determine the approximate time between a start and end time. By default,
+     * the start time is the current server time, given by `time()`. A string
+     * value of the approimate time is returned. e.g. 'in 5 hours'; '3 days ago'.
+     */
+    public static function approximateTime($end_time, $start_time = null)
+    {
+        if ($start_time === null) {
+            $start_time = time();
+        }
+        $seconds_diff = abs($end_time - $start_time);
+        $has_happened = $end_time > $start_time;
+        $start_datetime = new DateTime(date('Y-m-d H:i:s', $start_time));
+        $end_datetime = new DateTime(date('Y-m-d H:i:s', $end_time));
+        $interval = date_diff($start_datetime, $end_datetime);
+        $formats = [
+            ["%y", 'year'],
+            ["%m", 'month'],
+            ["%d", 'day'],
+            ["%h", 'hour'],
+            ['%i', 'minute']
+        ];
+        foreach ($formats as $format) {
+            $result = $interval->format($format[0]);
+            if ($result) {
+                $interval_string = "$result ". $format[1]. ($result > 1 ? 's' : '');
+                $return = $has_happened ? "$interval_string ago" : "In $interval_string";
+                return $return;
+            }
+        }
+        return "Just now";
     }
 }

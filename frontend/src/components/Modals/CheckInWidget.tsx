7e93a5f0c760ef620a8124ab990300a91d18cacb
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-12 21:20:24 -0700

    Convert CheckInWidget to func. comp.

--- frontend/src/components/Modals/CheckInWidget.tsx
+++ frontend/src/components/Modals/CheckInWidget.tsx
@@ -1,160 +1,142 @@
 import * as React from 'react'
-
 import * as classNames from 'classnames'
 
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import Checkbox from '@material-ui/core/Checkbox'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Dialog from '@material-ui/core/Dialog'
-import Grow from '@material-ui/core/Grow'
-import Icon from '@material-ui/core/Icon'
-import IconButton from '@material-ui/core/IconButton'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import Snackbar from '@material-ui/core/Snackbar'
-import Switch from '@material-ui/core/Switch'
-import TextField from '@material-ui/core/TextField'
-import Tooltip from '@material-ui/core/Tooltip'
+import {
+    Avatar,
+    Checkbox,
+    CircularProgress,
+    Dialog,
+    Fade,
+    Grow,
+    Icon,
+    IconButton,
+    InputAdornment,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemSecondaryAction,
+    Switch,
+    TextField,
+    Tooltip
+} from '@material-ui/core'
 
 import { NavItem } from '../Sidebar/NavItem'
+import { EnhancedDialogTitle } from './EnhancedDialogTitle'
 
-type AirCheckInStatus = 'on' | 'off' | 'loading'
+interface ReduxProps {}
 
-interface AirCheckIn {
-    name: string
-    day: string
-    time: string
-}
+interface IProps extends ReduxProps {}
 
-interface IState {
-    open: boolean
-    airCheckInStatus: AirCheckInStatus
-    checkInValue: string
-}
+export const CheckInWidget = (props: IProps) => {
+    // useStates
+    const [open, setOpen]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = React.useState(true)
+    const [airCheckInEnabled, setAirCheckInEnabled]: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+        = React.useState(false)
+    const [loadingAirStatus, setLoadingAirStatus]: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+        = React.useState(false)
+    const [loadingCheckIn, setLoadingCheckIn]: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+        = React.useState(false)
+    const [inputValue, setInputValue]: [string, React.Dispatch<React.SetStateAction<string>>]
+        = React.useState('')
 
-export class CheckInWidget extends React.Component<{}, IState> {
-    state = {
-        open: false,
-        airCheckInStatus: 'off' as AirCheckInStatus,
-        checkInValue: ''
+    const handleClickOpen = () => {
+        setOpen(true)
     }
 
-    handleClickOpen = () => {
-        this.setState({ open: true })
+    const handleClose = () => {
+        setOpen(false)
     }
 
-    handleClose = () => {
-        this.setState({ open: false })
+    const handleChange = (event: any) => {
+        setInputValue(event.target.value)
     }
 
-    handleChange = (event: any) => {
-        this.setState({ checkInValue: event.target.value})
+    const toggleAirCheckIn = () => {
+        setAirCheckInEnabled(!airCheckInEnabled)
     }
 
-    toggleAirCheckIn = () => {
-        switch (this.state.airCheckInStatus) {
-            case 'on': {
-                this.setState({ airCheckInStatus: 'loading' }, () => {
-                    setTimeout(() => {
-                        this.setState({ airCheckInStatus: 'off' })
-                    }, 500)
-                })
-                return
-            }
-            case 'off': {
-                this.setState({ airCheckInStatus: 'loading' }, () => {
-                    setTimeout(() => {
-                        this.setState({ airCheckInStatus: 'on' })
-                    }, 500)
-                })
-                return
-            }
-            default:
-                return
-        }
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        setLoadingCheckIn(true)
+        
     }
 
-    render() {
-        return (
-            <>
-                <NavItem
-                    title='Check-in'
-                    icon='how_to_reg'
-                    badgeCount={3}
-                    onClick={this.handleClickOpen}
-                />
-                <Dialog
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                    className='check-in_modal'
-                    scroll='paper'
-                >
-                    <div className='check-in_modal__header'>
-                        <h3>Student Check-in</h3>
-                        <Tooltip title='Close'>
-                            <IconButton className='icon-close' onClick={this.handleClose}>
-                                <Icon>close</Icon>
-                            </IconButton>
-                        </Tooltip>
+    return (
+        <>
+            <NavItem
+                title='Check-in'
+                icon='how_to_reg'
+                badgeCount={3}
+                onClick={() => handleClickOpen()}
+            />
+            <Dialog
+                open={open}
+                onClose={() => handleClose()}
+                className='check-in_modal'
+                scroll='paper'
+            >
+                <EnhancedDialogTitle title='Student Check-in' onClose={handleClose} />
+                <div className='check-in_modal__content'>
+                    <div className='check-in_heading'>
+                        <Icon>keyboard</Icon>
+                        <h4 className='heading_type'>Scan or Enter</h4>
                     </div>
-                    <div className='check-in_modal__content'>
-                        <div className='check-in_heading'>
-                            <Icon>keyboard</Icon>
-                            <h4 className='heading_type'>Scan or Enter</h4>
-                        </div>
-                        <div className='check-in-input'>
-                            <TextField
-                                name='check-in'
-                                type='text'
-                                placeholder='Enter Student Numbers'
-                                variant='outlined'
-                                value={this.state.checkInValue}
-                                onChange={this.handleChange}
-                                margin='normal'
-                                autoFocus={true}
-                                fullWidth={true}
-                                helperText='Comma separated list or single entry'
+                    <form className='check-in-input' onSubmit={handleSubmit}>
+                        <TextField
+                            name='check-in'
+                            type='text'
+                            placeholder='Enter Student Numbers'
+                            variant='outlined'
+                            value={inputValue}
+                            onChange={handleChange}
+                            margin='normal'
+                            autoFocus
+                            fullWidth
+                            helperText='Comma separated list or single entry'
+                            InputProps={{
+                                endAdornment: loadingCheckIn ? (
+                                    <div><CircularProgress size={24} /></div>
+                                ) : (
+                                    <InputAdornment position='end'>
+                                        <IconButton disabled={inputValue.length === 0} onClick={handleSubmit}><Icon>keyboard_return</Icon></IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </form>
+                    <div className='check-in_heading'>
+                            <Icon>wifi</Icon>
+                            <h4 className='heading_type'>Air Check-in</h4>
+                            <h3 className={classNames('heading_status', {'--online': airCheckInEnabled})}>
+                                {airCheckInEnabled ? 'Online' : 'Offline'}
+                            </h3>
+                            <Switch 
+                                checked={airCheckInEnabled}
+                                onChange={() => toggleAirCheckIn()}
+                                color='primary'
                             />
+                            <Grow in={loadingAirStatus}>
+                                <CircularProgress color='primary' disableShrink/>
+                            </Grow>
                         </div>
-                        <div className='check-in_heading'>
-                                <Icon>wifi</Icon>
-                                <h4 className='heading_type'>Air Check-in</h4>
-                                <h3 className={classNames(
-                                    'heading_status',
-                                    { '--online': this.state.airCheckInStatus === 'on'}
-                                )}>{this.state.airCheckInStatus === 'on' ? 'Online' : 'Offline'}</h3>
-                                <Switch 
-                                    checked={this.state.airCheckInStatus === 'on'}
-                                    onChange={this.toggleAirCheckIn}
-                                    color='primary'
-                                />
-                                <Grow in={this.state.airCheckInStatus === 'loading'}>
-                                    <CircularProgress color='primary' />
-                                </Grow>
-                            </div>
-                        <List dense className='air-check-ins'>
-                            <ListItem button>
-                                <ListItemAvatar><Avatar>CU</Avatar></ListItemAvatar>
-                                <span>Curtis Upshall</span>
-                                <ListItemSecondaryAction>
-                                    <Checkbox color='primary' />
-                                </ListItemSecondaryAction>
-                            </ListItem>
-                            <ListItem button>
-                                <ListItemAvatar><Avatar>VL</Avatar></ListItemAvatar>
-                                <span>Vlad Lyesin</span>
-                                <ListItemSecondaryAction>
-                                    <Checkbox color='primary' />
-                                </ListItemSecondaryAction>
-                            </ListItem>
-                        </List>
-                    </div>
-                </Dialog>
-            </>
-        )
-    }
+                    <List dense className='air-check-ins'>
+                        <ListItem button>
+                            <ListItemAvatar><Avatar>CU</Avatar></ListItemAvatar>
+                            <span>Curtis Upshall</span>
+                            <ListItemSecondaryAction>
+                                <Checkbox color='primary' />
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemAvatar><Avatar>VL</Avatar></ListItemAvatar>
+                            <span>Vlad Lyesin</span>
+                            <ListItemSecondaryAction>
+                                <Checkbox color='primary' />
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    </List>
+                </div>
+            </Dialog>
+        </>
+    )
 }

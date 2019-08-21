7307880866a6481e5ba165d1fe4742fef2e2148a
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-21 03:01:17 -0700

    More feedback widget options

--- frontend/src/components/Sidebar/HelpWidget.tsx
+++ frontend/src/components/Sidebar/HelpWidget.tsx
@@ -3,17 +3,33 @@ import * as React from 'react'
 import {
     Button,
     Checkbox,
+    Chip,
     Dialog,
     DialogActions,
     DialogContent,
+    DialogContentText,
     FormControlLabel,
     Menu,
     MenuItem,
-    TextField
+    TextField,
 } from '@material-ui/core'
 
 import { NavItem } from './NavItem'
 import { EnhancedDialogTitle } from '../Modals/EnhancedDialogTitle'
+
+interface IChip {
+    label: string,
+    value: string
+}
+
+const chips: IChip[] = [
+    { label: 'Unexpected Behavior', value: 'unexpected-behavior' },
+    { label: 'Confusing Feature', value: 'confusing-feature' },
+    { label: 'Missing Feature', value: 'missing-feature' },
+    { label: 'Permissions', value: 'permissions' },
+    { label: 'Connectivity', value: 'connectivity' },
+    { label: 'Other', value: 'other' }
+]
 
 export const HelpWidget = () => {
     const [menuRef, setMenuRef]: [any, React.Dispatch<React.SetStateAction<any>>]
@@ -22,6 +38,10 @@ export const HelpWidget = () => {
         = React.useState(false)
     const [inputValue, setInputValue]: [string, React.Dispatch<React.SetStateAction<string>>]
         = React.useState('')
+    const [selectedChips, setSelectedChips]: [string[], React.Dispatch<React.SetStateAction<string[]>>]
+        = React.useState([])
+    const [allowReply, setAllowReply]: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+    = React.useState(true)
     const menuOpen: boolean = Boolean(menuRef)
 
     const handleInputChange = (event: any) => {
@@ -45,6 +65,18 @@ export const HelpWidget = () => {
         setDialogOpen(false)
     }
 
+    const handleSelectChip = (value: string) => {
+        setSelectedChips([...selectedChips, value])
+    }
+
+    const handleDeselectChip = (value: string) => {
+        setSelectedChips(selectedChips.filter((chipValue: string) => chipValue !== value))
+    }
+
+    const toggleChecked = () => {
+        setAllowReply(!allowReply)
+    }
+
     const handleSubmit = (event: any) => {}
 
     return (
@@ -64,24 +96,38 @@ export const HelpWidget = () => {
             <Dialog open={dialogOpen}>
                 <EnhancedDialogTitle title='Provide Feedback' onClose={handleDialogClose}/>
                 <DialogContent>
+                    <DialogContentText>We take feedback seriously to ensure Spotlight meets your school's needs.</DialogContentText>
+                    <div className='chips_container'>
+                        {chips.map((chip: IChip) => {
+                            const selected: boolean = selectedChips.includes(chip.value)
+                            return (
+                                <Chip
+                                    onClick={() => {selected ? handleDeselectChip(chip.value) : handleSelectChip(chip.value)}}
+                                    variant={selected ? 'default' : 'outlined'}
+                                    label={chip.label}
+                                />
+                            )
+                        })}
+                    </div>
                     <TextField
                         value={inputValue}
                         onChange={handleInputChange}
+                        placeholder='Describe your issue or idea'
+                        label='Feedback'
                         variant='filled'
                         fullWidth
                         autoFocus
                         multiline
-                        placeholder='Describe your issue or idea'
                     />
                     <FormControlLabel
                         control={
                             <Checkbox
-                                checked={true}
-                                onChange={() => {}}
+                                checked={allowReply}
+                                onChange={() => toggleChecked()}
                                 color='primary'
                             />
                         }
-                        label='I may be contacted in response to my feedback'
+                        label='Allow Spotlight to contacted me in response to my feedback'
                     />
                 </DialogContent>
                 <DialogActions>

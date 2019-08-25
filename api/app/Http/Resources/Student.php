22de777e937a9700441a8939b02893fc3253d090
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-25 10:37:01 -0700

    Add name field to Student resource

--- api/app/Http/Resources/Student.php
+++ api/app/Http/Resources/Student.php
@@ -14,6 +14,13 @@ class Student extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'student_number' => $this->student_number,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'name' => $this->getName(),
+            'initials' => $this->initials
+        ];
     }
 }

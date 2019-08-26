edf0cb6d71bd93fb8030a441a6ccdbeb0c6ed824
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-25 20:44:18 -0700

    block_topics only on flex blocks

--- api/database/seeds/TopicsTableSeeder.php
+++ api/database/seeds/TopicsTableSeeder.php
@@ -23,7 +23,10 @@ class TopicsTableSeeder extends Seeder
         $end_time = strtotime('+28 days', $time);
         do {
             $day_of_week = date('w', $time) + 1;
-            $blocks = App\BlockSchedule::where('day_of_week', $day_of_week)->get();
+            $blocks = App\BlockSchedule::filter(function($block_schedule) use ($day_of_week) {
+                return $block_schedule->day_of_week === $day_of_week
+                && $block_schedule->block()->first()->flex == true;
+            });
             $staff_list->each(function($staff) use ($blocks, $time) {
                 $blocks->each(function($block) use ($time, $staff) {
                     $topic = $staff->getTopics()->random();

5f15a2f8a4268f5f285f6a6443adcac4cd794c7b
Curtis Upshall <curtisupshall@uvic.ca>
2019-08-29 13:11:31 -0700

    Temporarily use local dev React

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,8 @@
     <body>
         <div id="app-root"></div>
         <!-- Dependencies -->
-        <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
-        <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+        <script src="{{ asset('js/react.development.js') }}"></script>
+        <script src="{{ asset('js/react-dom.development.js') }}"></script>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
         <!-- Main -->

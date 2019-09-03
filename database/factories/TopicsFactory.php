cbf379ea05abae5d523aa37b5095cb73260f81c7
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-02 21:43:29 -0700

    Fix Topics seeder after renaming  field to

--- database/factories/TopicsFactory.php
+++ database/factories/TopicsFactory.php
@@ -5,7 +5,7 @@ use App\Http\Utils;
 
 $factory->define(App\Topic::class, function (Faker $faker) {
     return [
-        'topic' => $faker->sentence(rand(1, 5)),
+        'memo' => $faker->sentence(rand(1, 5)),
         'color' => Utils::topicColor()
     ];
 });

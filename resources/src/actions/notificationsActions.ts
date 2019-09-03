3690b1c7aa1bec05bf2d420dfdff0c0697abb91a
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-02 22:51:06 -0700

    Create additional notifications reducer logic

--- resources/src/actions/notificationsActions.ts
+++ resources/src/actions/notificationsActions.ts
@@ -10,6 +10,7 @@ import {
     UNARCHIVE_NOTIFICATION
 } from './types'
 import { ReduxAction } from '../types/app'
+import { INotification } from '../types/staff';
 
 export const archiveAllNotifications = () => {
     return (dispatch: (action: ReduxAction<void>) => void) => {
@@ -21,12 +22,12 @@ export const archiveAllNotifications = () => {
     }
 }
 
-export const archiveNotification = (notificationID: number) => {
-    return (dispatch: (action: ReduxAction<void>) => void) => {
-        axios.put(`/api/notifications/archive/${notificationID}`)
+export const archiveNotification = (notification: INotification) => {
+    return (dispatch: (action: ReduxAction<INotification>) => void) => {
+        axios.put(`/api/notifications/archive/${notification.id}`)
         return dispatch({
             type: ARCHIVE_NOTIFICATION,
-            payload: null
+            payload: notification
         })
     }
 }
@@ -54,32 +55,32 @@ export const markAllNotificationsAsRead = () => {
     }
 }
 
-export const markNotificationAsRead = (notificationID: number) => {
-    return (dispatch: (action: ReduxAction<void>) => void) => {
-        axios.put(`/api/notifications/read/${notificationID}`)
+export const markNotificationAsRead = (notification: INotification) => {
+    return (dispatch: (action: ReduxAction<INotification>) => void) => {
+        axios.put(`/api/notifications/read/${notification.id}`)
         return dispatch({
             type: MARK_NOTIFICATION_READ,
-            payload: null
+            payload: notification
         })
     }
 }
 
-export const markNotificationAsUnread = (notificationID: number) => {
-    return (dispatch: (action: ReduxAction<void>) => void) => {
-        axios.put(`/api/notifications/unread/${notificationID}`)
+export const markNotificationAsUnread = (notification: INotification) => {
+    return (dispatch: (action: ReduxAction<INotification>) => void) => {
+        axios.put(`/api/notifications/unread/${notification.id}`)
         return dispatch({
             type: MARK_NOTIFICATION_UNREAD,
-            payload: null
+            payload: notification
         })
     }
 }
 
-export const unarchiveNotification = (notificationID: number) => {
-    return (dispatch: (action: ReduxAction<void>) => void) => {
-        axios.put(`/api/notifications/unarchive/${notificationID}`)
+export const unarchiveNotification = (notification: INotification) => {
+    return (dispatch: (action: ReduxAction<INotification>) => void) => {
+        axios.put(`/api/notifications/unarchive/${notification.id}`)
         return dispatch({
             type: UNARCHIVE_NOTIFICATION,
-            payload: null
+            payload: notification
         })
     }
 }
--- resources/src/reducers/notificationReducer.ts
+++ resources/src/reducers/notificationReducer.ts
@@ -1,5 +1,15 @@
-import { FETCH_NOTIFICATIONS } from '../actions/types'
+import {
+    ARCHIVE_ALL_NOTIFICATIONS,
+    ARCHIVE_NOTIFICATION,
+    FETCH_NOTIFICATIONS,
+    MARK_ALL_NOTIFICATIONS_READ,
+    MARK_NOTIFICATION_READ,
+    MARK_NOTIFICATION_UNREAD,
+    UNARCHIVE_NOTIFICATION
+} from '../actions/types'
+
 import { INotification } from '../types/staff'
+import { ReduxAction } from '../types/app'
 
 interface IState {
     items: INotification[]
@@ -9,17 +19,42 @@ const initialState: IState = {
     items: []
 }
 
-interface IAction {
-    type: string,
-    payload: INotification[]
-}
-
-export const notificationReducer = (state = initialState, action: IAction) => {
+export const notificationReducer = (state = initialState, action: ReduxAction<INotification>) => {
     switch (action.type) {
+        case ARCHIVE_ALL_NOTIFICATIONS:
+            return {
+                items: []
+            }
+        case ARCHIVE_NOTIFICATION:
+            return {
+                items: state.items.filter((item: INotification) => {
+                    return item.id !== action.payload.id
+                })
+            }
         case FETCH_NOTIFICATIONS:
             return {
                 items: action.payload
             }
+        case MARK_ALL_NOTIFICATIONS_READ:
+            return {
+                items: state.items.map((item: INotification) => {
+                    return { ...item, read: true }
+                })
+            }
+        case MARK_NOTIFICATION_READ:
+            return {
+                items: state.items.map((item: INotification) => {
+                    return item.id === action.payload.id ?  { ...item, read: true } : item
+                })
+            }
+        case MARK_NOTIFICATION_UNREAD:
+            return {
+                items: state.items.map((item: INotification) => {
+                    return item.id === action.payload.id ?  { ...item, read: false } : item
+                })
+            }
+        case UNARCHIVE_NOTIFICATION:
+            return state
         default:
             return state
     }

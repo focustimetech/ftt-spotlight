ba281533c89182ad20886bf8646370d7ae6186f8
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-10 23:55:00 -0700

    SPOT-21: Add password change axios method

--- resources/src/utils/http.ts
+++ resources/src/utils/http.ts
@@ -8,3 +8,15 @@ import axios from 'axios'
 export const verifyPassword = (password: string): Promise<void> => {
     return axios.post('/api/verify-user', { password })
 }
+
+/**
+ * Changes the authenticated user's password
+ * @param oldPassword The user's old password, which is verified by the server
+ * @param newPassword The user's new password
+ */
+export const changePassword = (oldPassword: string, newPassword: string): Promise<any> => {
+    return axios.post('/api/change-password', {
+        old_password: oldPassword,
+        new_Password: newPassword
+    })
+}

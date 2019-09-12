39a81df53b04f795d973856453b034fe0e43e7cc
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-12 16:03:24 -0700

    SPOT-25: Pull user from Redux store in AccountWidget

--- resources/src/components/Sidebar/AccountWidget.tsx
+++ resources/src/components/Sidebar/AccountWidget.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import * as classNames from 'classnames'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import {
     Avatar,
@@ -7,18 +10,21 @@ import {
 } from '@material-ui/core'
 
 import { NavItem } from '../Sidebar/NavItem'
+import { IUser } from '../../types/auth'
+
+interface ReduxProps {
+    currentUser: IUser
+}
+
+interface IProps extends ReduxProps {
+    onSignOut: (callback?: () => void) => void
+}
 
 interface IState {
     menuRef: any
 }
 
-interface IProps {
-    onSignOut: (callback?: () => void) => void
-    initials: string
-    background: string
-}
-
-export class AccountWidget extends React.Component<IProps, IState> {
+class AccountWidget extends React.Component<IProps, IState> {
     state: IState = {
         menuRef: null
     }
@@ -33,22 +39,33 @@ export class AccountWidget extends React.Component<IProps, IState> {
 
     render() {
         const { menuRef } = this.state
-        const { initials, background } = this.props
+        const { initials, color } = this.props.currentUser.details
         const menuOpen = Boolean(menuRef)
+        const profileLink = this.props.currentUser.account_type === 'staff'
+            ? `/staff/${this.props.currentUser.details.id}`
+            : `/students/${this.props.currentUser.details.id}`
+
         return (
             <>
                 <NavItem className='nav_account' title='Account' onClick={this.handleClickOpen}>
-                    <Avatar className='nav_avatar' style={{ background }}>{initials}</Avatar>
+                    <Avatar className={classNames('nav_avatar', `--${color}`)}>{initials}</Avatar>
                 </NavItem>
                 <Menu
                     open={menuOpen}
                     anchorEl={menuRef}
                     onClose={this.handleClose}
                 >
-                    <MenuItem>Profile</MenuItem>
+                    <Link to={profileLink}><MenuItem>Profile</MenuItem></Link>
                     <MenuItem onClick={() => this.props.onSignOut()}>Sign Out</MenuItem>
                 </Menu>
             </>
         )
     }
 }
+
+const mapStateToProps = (state: any) => ({
+    currentUser: state.auth.user
+})
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountWidget)

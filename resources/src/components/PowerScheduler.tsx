5e561106025972ae7aed097373dd58990e9c9b78
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-14 19:25:03 -0700

    SPOT-28: Move PowerScheduler from Dialog to main screen

--- resources/src/components/PowerScheduler.tsx
+++ resources/src/components/PowerScheduler.tsx
@@ -2,46 +2,174 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 
 import {
-    Dialog,
-    Button
+    Button,
+    DialogContent,
+    Step,
+    StepContent,
+    StepLabel,
+    Stepper,
+
 } from '@material-ui/core'
 
-import CreatePowerScheduleForm from './Form/CreatePowerScheduleForm'
-import { EnhancedDialogTitle } from './Modals/EnhancedDialogTitle'
+import { TopNav } from './TopNav'
 import { EnhancedTable } from './Table/EnhancedTable'
-import { IUser } from '../types/auth'
-import { EmptyStateIcon } from './EmptyStateIcon'
-
+import { fetchStudents } from '../actions/studentActions'
+import { IStudent } from '../types/student'
+import { ITableHeaderColumn } from '../types/table'
 
 interface ReduxProps {
-    // currentUser: IUser
+    students: IStudent[]
+    fetchStudents: () => Promise<any>
 }
 
-interface IProps extends ReduxProps {}
+interface IProps extends ReduxProps {
+    onSubmit: () => Promise<any>
+}
 
 interface IState {
-    createDialogOpen: boolean
+    loadingStudents: boolean
+    selectedStudents: number[]
+    step: number
+    uploading: boolean
 }
 
-class PowerScheduler extends React.Component<IProps, IState> {
+class CreatePowerScheduleForm extends React.Component<IProps, IState> {
     state: IState = {
-        createDialogOpen: false
+        selectedStudents: [],
+        loadingStudents: false,
+        step: 0,
+        uploading: false
+    }
+
+    handleNextStep = () => {
+        this.setState((state: IState) => ({
+            step: state.step + 1
+        }))
+    }
+
+    handlePreviousStep = () => {
+        this.setState((state: IState) => ({
+            step: state.step > 0 ? state.step - 1 : 0
+        }))
+    }
+
+    handleSetSelected = (selectedStudents: number[]) => {
+        this.setState({ selectedStudents })
+    }
+
+    componentDidMount() {
+        this.setState({ loadingStudents: true })
+        this.props.fetchStudents()
+            .then(() => {
+                this.setState({ loadingStudents: false })
+            })
     }
 
     render() {
+        const studentTableColumns: ITableHeaderColumn[] = [
+            {
+                id: 'name',
+                label: 'Name',
+                th: true,
+                isNumeric: false,
+                disablePadding: true,
+                searchable: false,
+                filterable: false,
+                visible: true
+            },
+            {
+                id: 'last_name',
+                label: 'Last Name',
+                isNumeric: false,
+                searchable: true,
+                filterable: true,
+                visible: false
+            },
+            {
+                id: 'first_name',
+                label: 'First Name',
+                isNumeric: false,
+                searchable: true,
+                filterable: true,
+                visible: false
+            },
+            {
+                id: 'grade',
+                label: 'Grade',
+                th: false,
+                isNumeric: true,
+                disablePadding: false,
+                searchable: false,
+                filterable: true,
+                visible: true
+            }
+        ]
+
+        const students = this.props.students ? (
+            this.props.students.map((student: IStudent) => {
+                return {
+                    id: student.id,
+                    name: student.name,
+                    last_name: student.last_name,
+                    first_name: student.first_name,
+                    grade: student.grade
+                }
+            })
+        ) : []
+
         return (
-			<div className='content --content-inner' id='content'>
-                <Dialog open={true}>
-                    <EnhancedDialogTitle title='Create Power Schedule' />
-                    <CreatePowerScheduleForm onSubmit={() => null}/>
-                </Dialog>
-                <EmptyStateIcon variant='flash'>
-                    <h2>Your Power Scheduler is empty</h2>
-                    <Button variant='contained' color='primary'>Create</Button>
-                </EmptyStateIcon>
-            </div>
+            <>
+                <div className='content' id='content'>
+                    <TopNav><h3>Power Scheduler</h3></TopNav>
+                    <Stepper activeStep={this.state.step} orientation='vertical'>
+                        <Step key={0}>
+                            <StepLabel>Select Students</StepLabel>
+                            <StepContent>
+                                <p>Select students from the table below.</p>
+                                <EnhancedTable
+                                    title='Students'
+                                    loading={this.state.loadingStudents}
+                                    columns={studentTableColumns}
+                                    data={students}
+                                    onSelect={this.handleSetSelected}
+                                    selected={this.state.selectedStudents}
+                                    searchable
+                                />
+                                <div className='stepper-actions'>
+                                    <Button
+                                        disabled={this.state.selectedStudents.length === 0}
+                                        variant='contained'
+                                        color='primary'
+                                        onClick={() => this.handleNextStep()}
+                                    >Next</Button>
+                                </div>
+                            </StepContent>
+                        </Step>
+                        <Step key={1}>
+                            <StepLabel>Select Date and Blocks</StepLabel>
+                            <StepContent>
+                                <p>Select date and blocks.</p>
+                                <div className='stepper-actions'>
+                                    <Button variant='text' onClick={() => this.handlePreviousStep()}>Back</Button>
+                                    <Button
+                                        variant='contained'
+                                        color='primary'
+                                        onClick={() => this.handleNextStep()}
+                                    >Next</Button>
+                                </div>
+                            </StepContent>
+                        </Step>
+                    </Stepper>
+                </div>
+            </>
         )
     }
 }
 
-export default PowerScheduler
+const mapDispatchToProps = { fetchStudents }
+
+const mapStateToProps = (state: any) => ({
+    students: state.students.items
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreatePowerScheduleForm)

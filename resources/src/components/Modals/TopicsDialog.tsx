0bbf4108109a0bd93d85ad5bae96434b14766491
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-17 21:50:54 -0700

    SPOT-32: Random starting Topic color on create Topic

--- resources/src/components/Modals/TopicsDialog.tsx
+++ resources/src/components/Modals/TopicsDialog.tsx
@@ -16,7 +16,7 @@ import {
 } from '@material-ui/core'
 
 import { ITopic } from '../../types/calendar'
-import { TopicColor } from '../../theme'
+import { TopicColor, getRandomColor } from '../../theme'
 import { ITopicRequest } from '../../actions/topicActions'
 import { EmptyStateIcon } from '../EmptyStateIcon'
 import { LoadingButton } from '../Form/LoadingButton';
@@ -26,10 +26,10 @@ import { createTopic, deleteTopic, fetchTopics} from '../../actions/topicActions
 import { isEmpty } from '../../utils/utils'
 import { ConfirmationDialog } from './ConfirmationDialog';
 
-const emptyTopic: ITopicRequest = {
+const emptyTopic = (): ITopicRequest => ({
     memo: '',
-    color: 'blue'
-}
+    color: getRandomColor().name
+})
 
 interface ReduxProps {
     topics: ITopic[]
@@ -65,7 +65,7 @@ class TopicsDialog extends React.Component<IProps, IState> {
         newTopicErrored: false,
         newTopicOpen: false,
         colorDialogOpen: false,
-        newTopic: emptyTopic,
+        newTopic: emptyTopic(),
         deleteTopicDialogOpen: false,
         deleteTopic: { id: 0, color: null, deleted: false, memo: '', staff: null }
     }
@@ -88,7 +88,7 @@ class TopicsDialog extends React.Component<IProps, IState> {
                 this.setState({
                     loadingNewTopic: false,
                     newTopicOpen: false,
-                    newTopic: emptyTopic
+                    newTopic: emptyTopic()
                 })
             })
             .catch((error: any) => {

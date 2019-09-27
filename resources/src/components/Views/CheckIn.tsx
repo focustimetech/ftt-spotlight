f9484f4080e23869137dfbf4979d03723487a4c3
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-27 00:17:28 -0700

    SPOT-63: Implement CheckInForm

--- resources/src/components/Views/CheckIn.tsx
+++ resources/src/components/Views/CheckIn.tsx
@@ -13,6 +13,7 @@ import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers'
 
 import { fetchCheckInStatus } from '../../actions/checkinActions'
 import { LoadingButton } from '../Form/LoadingButton'
+import CheckInForm from '../Form/CheckInForm'
 import { TopNav } from '../TopNav'
 import { CheckInStatus } from '../../types/checkin'
 
@@ -26,12 +27,14 @@ interface IProps extends ReduxProps {}
 interface IState {
     date: Date
     datePickerOpen: boolean
+    loadingStatus: boolean
 }
 
 class CheckIn extends React.Component<IProps, IState> {
     state: IState = {
         date: new Date(),
-        datePickerOpen: false
+        datePickerOpen: false,
+        loadingStatus: false
     }
 
     handleDatePickerOpen = () => {
@@ -57,52 +60,63 @@ class CheckIn extends React.Component<IProps, IState> {
     fetchToday = () => {}
 
     componentDidMount() {
-
+        this.setState({ loadingStatus: true })
+        this.props.fetchCheckInStatus()
+            .then(() => {
+                this.setState({ loadingStatus: false })
+            })
     }
 
     render() {
         return (
             <div className='content' id='content'>
                 <TopNav breadcrumbs={[{ value: 'Check-in' }]} />
-                <ul className='calendar_header'>
-                    <li>
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <DatePicker
-                                variant='dialog'
-                                open={this.state.datePickerOpen}
-                                onClose={this.handleDatePickerClose}
-                                value={this.state.date}
-                                onChange={this.handleDatePickerSelect}
-                                TextFieldComponent={() => null}
-                            />
-                        </MuiPickersUtilsProvider>
-                        <Button onClick={() => this.handleDatePickerOpen()}>
-                            {'Select Date'}
-                        </Button>
-                    </li>
-                    <li>
-                        <Tooltip title='Back' placement='top'>
-                            <IconButton onClick={() => this.fetchPrevious()}>
-                                <Icon>chevron_left</Icon>
-                            </IconButton>
-                        </Tooltip>
-                    </li>
-                    <li>
-                        <Tooltip title='Next' placement='top'>
-                            <IconButton onClick={() => this.fetchNext()}>
-                                <Icon>chevron_right</Icon>
-                            </IconButton>
-                        </Tooltip>
-                    </li>
-                    <li>
-                        <Button
-                            variant='text'
-                            color='primary'
-                            onClick={() => this.fetchToday()}
-                            disabled={this.props.checkInStatus.date.is_today}
-                        >Today</Button>
-                    </li>
-                </ul>
+                {this.state.loadingStatus && this.props.checkInStatus ? (
+                    <h2>Loading</h2>
+                ) : (
+                    <>
+                        <ul className='calendar_header'>
+                            <li>
+                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <DatePicker
+                                        variant='dialog'
+                                        open={this.state.datePickerOpen}
+                                        onClose={this.handleDatePickerClose}
+                                        value={this.state.date}
+                                        onChange={this.handleDatePickerSelect}
+                                        TextFieldComponent={() => null}
+                                    />
+                                </MuiPickersUtilsProvider>
+                                <Button onClick={() => this.handleDatePickerOpen()}>
+                                    {'Select Date'}
+                                </Button>
+                            </li>
+                            <li>
+                                <Tooltip title='Back' placement='top'>
+                                    <IconButton onClick={() => this.fetchPrevious()}>
+                                        <Icon>chevron_left</Icon>
+                                    </IconButton>
+                                </Tooltip>
+                            </li>
+                            <li>
+                                <Tooltip title='Next' placement='top'>
+                                    <IconButton onClick={() => this.fetchNext()}>
+                                        <Icon>chevron_right</Icon>
+                                    </IconButton>
+                                </Tooltip>
+                            </li>
+                            <li>
+                                <Button
+                                    variant='text'
+                                    color='primary'
+                                    onClick={() => this.fetchToday()}
+                                    disabled={false /*this.props.checkInStatus.date.is_today*/}
+                                >Today</Button>
+                            </li>
+                        </ul>
+                        <CheckInForm />
+                    </>
+                )}
             </div>
         )
     }

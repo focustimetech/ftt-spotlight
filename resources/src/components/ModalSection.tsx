e46109805585bb5e9e123f5364d857c9ee6be903
Curtis Upshall <curtisupshall@uvic.ca>
2019-09-27 21:43:28 -0700

    SPOT-63: Create ModalSection

---
+++ resources/src/components/ModalSection.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+
+import { Button, Icon, Typography } from '@material-ui/core'
+import { SetState } from '../types/app'
+
+interface IProps {
+    children: any
+    icon: string
+    title: string
+    collapsible?: boolean
+    count?: boolean
+    labelAdornment?: any
+    open?: boolean
+    onOpen?: () => void
+    onClose?: () => void
+}
+
+export const ModalSection = (props: IProps) => {
+    const [open, setOpen]: [boolean, SetState<boolean>] = React.useState(true)
+    const isOpen: boolean = props.open !== false && (props.open || open)
+
+    const handleHeaderClick = () => {
+        if (props.collapsible === false)
+            return
+        if (open && props.onClose)
+            props.onClose()
+        else if (!open && props.onOpen)
+            props.onOpen()
+        setOpen(!open)
+    }
+
+    return (
+        <div className='modal-section'>
+            <div className='modal-section__header'>
+                <Button
+                    variant='text'
+                    color='inherit'
+                    onClick={handleHeaderClick}
+                    className='modal-section__button'
+                >
+                    <Icon>{props.icon}</Icon>
+                    <Typography className='button_text'>{props.title}</Typography>
+                </Button>
+                {props.labelAdornment}
+            </div>
+            <div className='modal-section__content'>
+                {props.children}
+            </div>
+        </div>
+    )
+}

cf2854c5b9b0817e69a8775912d123621cc73e81
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-22 14:58:33 -0700

    SPOT-142: Move date type defs to separate file

--- resources/src/types/report.d.ts
+++ resources/src/types/report.d.ts
@@ -1,3 +1,5 @@
+import { DateRange } from './date'
+
 export interface IReport {
     // id?: number
     name: string
@@ -12,41 +14,7 @@ export type ReportSegment =
     | 'week'
     | 'month'
 
-export type PredefinedDateRange =
-    | 'last-hour'
-    | 'last-24-hours'
-    | 'yesterday'
-    | 'today'
-    | 'last-2-days'
-    | 'last-7-days'
-    | 'last-14-days'
-    | 'last-30-days'
-    | 'this-week'
-    | 'last-week'
-    | 'this-month'
-    | 'last-month'
-    | 'all-time'
-
 export type ReportingState = 'idle' | 'running' | 'errored'
-
-export interface IAbsoluteDateRange {
-    type: 'absolute'
-    start: Date
-    end: Date
-}
-
-export interface IRelativeDateRange {
-    type: 'relative'
-    value: number
-    segment: ReportSegment
-}
-
-export interface IPredefinedDateRange {
-    type: 'predefined'
-    range: PredefinedDateRange
-}
-
-export type DateRange = IAbsoluteDateRange | IRelativeDateRange | IPredefinedDateRange
 
 export type ReportGroup = 
     | 'staff'

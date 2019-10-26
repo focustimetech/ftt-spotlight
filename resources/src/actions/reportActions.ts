14629b60e3d78db89b7d01835bfa7b5984f2161f
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-25 18:23:47 -0700

    SPOT-142: Correct actions paths

--- resources/src/actions/reportActions.ts
+++ resources/src/actions/reportActions.ts
@@ -11,7 +11,7 @@ import { IReport } from '../types/report'
 
 export const fetchReports = () => {
     return (dispatch: any) => {
-        return axios.get('/reports/self')
+        return axios.get('/api/reports/self')
             .then((res: any) => dispatch({
                 type: FETCH_REPORTS,
                 payload: res.data
@@ -21,7 +21,7 @@ export const fetchReports = () => {
 
 export const createReport = (report: IReport) => {
     return (dispatch: any) => {
-        return axios.post('/reports', report)
+        return axios.post('/api/reports', report)
             .then((res: any) => dispatch({
                 type: CREATE_REPORT,
                 payload: res.data
@@ -31,7 +31,7 @@ export const createReport = (report: IReport) => {
 
 export const updateReport = (report: IReport) => {
     return (dispatch: any) => {
-        return axios.put('/reports', report)
+        return axios.put('/api/reports', report)
             .then((res: any) => dispatch({
                 type: UPDATE_REPORT,
                 payload: res.data
@@ -41,7 +41,7 @@ export const updateReport = (report: IReport) => {
 
 export const deleteReport = (reportID: number) => {
     return (dispatch: any) => {
-        return axios.delete(`/reports/${reportID}`)
+        return axios.delete(`/api/reports/${reportID}`)
             .then((res: any) => dispatch({
                 type: DELETE_REPORT,
                 payload: res.data

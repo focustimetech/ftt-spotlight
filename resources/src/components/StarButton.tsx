ac43cb23959639d50baa5e9d2edf00fd46e9bd05
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-27 20:06:47 -0700

    SPOT-142: Add disabled prop, rounded icon to StarButton

--- resources/src/components/StarButton.tsx
+++ resources/src/components/StarButton.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react'
-import { Icon, IconButton } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+import StarBorderRoundedIcon from '@material-ui/icons/StarBorderRounded'
+import StarRoundedIcon from '@material-ui/icons/StarRounded'
 
 interface IProps {
+    disabled?: boolean
     isStarred: boolean,
     onClick: () => void
 }
 
 export const StarButton = (props: IProps) => {
     return (
-        <IconButton onClick={() => props.onClick()}>
-            <Icon className={props.isStarred ? '--starred': ''}>
-                {props.isStarred ? 'star' : 'star_border'}
-            </Icon>
+        <IconButton onClick={() => props.onClick()} disabled={props.disabled}>
+            {props.isStarred ? (
+                <StarRoundedIcon className='--starred' />
+            ) : (
+                <StarBorderRoundedIcon />
+            )}
         </IconButton>
     )
 }

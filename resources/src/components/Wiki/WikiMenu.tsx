e89f7a432a47ca727cc6fdcc2c8f05adb5e58b16
Curtis Upshall <curtisupshall@uvic.ca>
2019-10-28 23:14:09 -0700

    SPOT-155: Create WikiMenu.tsx

---
+++ resources/src/components/Wiki/WikiMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { ListItem } from '@material-ui/core'
+
+const WikiMenu = () => {
+    return (
+        <ul className='menu_list'>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+        </ul>
+    )
+}
+
+export { WikiMenu }

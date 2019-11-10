8203a71a99fd0fc79582ade52144177a4ddc232d
Curtis Upshall <curtisupshall@uvic.ca>
2019-11-10 09:44:34 -0800

    SPOT-150: Add default Accept and Content-Type request headers

--- resources/src/index.tsx
+++ resources/src/index.tsx
@@ -6,12 +6,14 @@ import { ThemeProvider } from '@material-ui/styles'
 
 import { store } from './store'
 import { theme } from './theme'
+import { setJsonHeaders } from './utils/api'
 import { setAuthorizationToken } from './utils/setAuthorizationToken'
 import { ACCESS_TOKEN } from './utils/storage'
 
 import AppWithAuth from './components/App/AppWithAuth'
 
 setAuthorizationToken(localStorage.getItem(ACCESS_TOKEN))
+setJsonHeaders()
 
 ReactDOM.render (
 	<ThemeProvider theme={theme}>

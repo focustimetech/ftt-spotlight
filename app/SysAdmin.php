e68a830df540a8ca467a5340cd298c29af4deced
Curtis Upshall <curtisupshall@uvic.ca>
2019-11-12 20:58:53 -0800

    SPOT-163: Create script for default SysAdmin

--- app/SysAdmin.php
+++ app/SysAdmin.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SysAdmin extends Model
 {
+    public $timestamps = true;
+    protected $table = 'sysadmins';
+    protected $fillable = [
+        'name', 'email', 'initials', 'color'
+    ];
+
     public function user()
 	{
 		return $this->hasOne('App\User', 'user_id')->where('account_type', 'sysadmin')->first();

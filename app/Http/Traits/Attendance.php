85758e3db451f9d55e34a436fb3fbe25b08191e0
Curtis Upshall <curtisupshall@uvic.ca>
2019-12-04 23:46:00 -0800

    SPOT-153: Finalize attendance report design

--- app/Http/Traits/Attendance.php
+++ app/Http/Traits/Attendance.php
@@ -31,8 +31,12 @@ trait Attendance
             $segment = 'day';
         }
 
-        $block_schedule = BlockSchedule::flexBlocks();
-        $num_blocks = $block_schedule->length(); // Number of BlockSchedules
+        /**
+         * A dictionary is constructed which uses the day of the week as the key. i.e.:
+         * { [ day_of_week ] => [ block1, block2, ... ], ... }
+         */
+        $block_schedule = BlockSchedule::flexBlocks()->sortBy('start')->groupBy('day_of_week');
+
         $students = collect();
         $found_student_ids = [];
         foreach ($student_ids as $student_id) {
@@ -48,22 +52,27 @@ trait Attendance
 
         // Set up time iterator
         $time = $start;
+        $date = date('Y-m-d', $time);
         $day_of_week = date('w', $start) + 1;
-        $segment_end = $start;
         
-        // Set up pointers
-        for ($block_pointer = 0; $block_schedule[$block_pointer]->start <= $time && $block_schedule[$block_pointer]->end > $time; $block_pointer ++);
+        // Set up block pointer
+        $block_pointer = 0;
+        while ($block_schedule[$day_of_week][$block_pointer]->start <= $time && $block_schedule[$day_of_week][$block_pointer]->end > $time) {
+            $block_pointer ++;
+        }
 
         // Set up attendance report
         $attendance = [
             'start' => date('Y-m-d H:i:s', $start),
             'end' => date('Y-m-d H:i:s', $end),
-            'total' => null,
-            'attended' => null,
-            'missed' => null,
+            'total' => 0,
+            'attended' => 0,
+            'missed' => 0,
+            'percentage' => 0,
             'segments' => []
         ];
 
+        $segment_end = $start;
         while ($time < $end) {
             // Set up a new segment
             $segment_start = $segment_end;
@@ -76,15 +85,43 @@ trait Attendance
             // Calculate the total number of blocks in the segment
             $block_count = 0;
             while ($time < $segment_end) {
-                $block_count ++;
-                $block_pointer = ($block_pointer + 1) % $num_blocks; // Increment the block pointer
-                $time = $block_schedule[$block_pointer]->start;
+                $num_blocks = count($block_schedule[$day_of_week]);
+                while ($time < $segment_end && $block_pointer < $num_blocks) {
+                    $time  = strtotime("$date " . $block_schedule[$day_of_week][$block_pointer]);
+                    $block_count ++;
+                    $block_pointer ++; // Increment the block pointer
+                }
+                // Advance the date
+                $block_pointer = 0;
+                $date = date('Y-m-d', strtotime('+1 day', $date));
+                $day_of_week = date('w', strtotime($date)) + 1;
             }
-
+            $segment_start_datetime = date('Y-m-d H:i:s', $segment_start);
+            $segment_end_datetime = date('Y-m-d H:i:s', $segment_end);
             $total = $block_count * $num_students;
-            $attended = 
+            $attended = $ledger_entries
+                ->where('checked_in_at', '>=', $segment_start_datetime)
+                ->where('checked_in_at', '<', $segment_end_datetime);
+            $segment = [
+                'start' => $segment_start_datetime,
+                'end' => $segment_end_datetime,
+                'total' => $total,
+                'attended' => $attended,
+                'missed' => $total - $attended,
+                'percentage' => $attended / $total * 100
+            ];
+
+            array_push($attendance['segments'], $segment);
         }
-        
+
+        foreach($attendance['segments'] as $segment) {
+            $attendance['total'] += $segment['total'];
+            $attendance['attended'] += $segment['attended'];
+        }
+        $attendance['missed'] = $attendance['total'] - $attendance['attended'];
+        $attendance['percentage'] = $attendance['attended'] / $attendance['total'] * 100;
+
+        return $attendance;
     }
 
     private static function getNextSegment($time, $segment) {

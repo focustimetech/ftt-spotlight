1bb981adea1bb90077afc26d3d69b8fcdd12c923
Curtis Upshall <curtisupshall@uvic.ca>
2020-04-04 22:09:03 -0700

    SPOT-197: Add meta description

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -8,9 +8,9 @@ import Document, {
 } from 'next/document'
 
 class SpotlightDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+    static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(context)
+        return initialProps
     }
 
     render() {
@@ -22,6 +22,8 @@ class SpotlightDocument extends Document {
             <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
             <meta name='msapplication-TileColor' content='#034670' />
             <meta name='theme-color' content='#034670' />
+            <meta name='description' content='Start using powerful tools that let your self-directed study blocks succeed.' />
+            <title>Spotlight - Smart attendance.</title>
             <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'></link>
             <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' />
             </Head>

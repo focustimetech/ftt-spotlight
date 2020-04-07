b0affd91155e7b62896cd81334cb3b376e91f614
Curtis Upshall <curtisupshall@uvic.ca>
2020-04-07 01:14:00 -0700

    SPOT-202: Add settings function to Utils

--- api2/app/Utils.php
+++ api2/app/Utils.php
@@ -1,11 +1,29 @@
 <?php
 
-namespace App\Utils;
+namespace App;
+
+use Arr;
 
 /**
  * A basic class for utilities accessed using public static methods.
  */
 class Utils
 {
-    //
+    public static function randomColor()
+    {
+        $colors = ['F44336', 'E91E63', '9C27B0', '673AB7', '3F51B5', '2196F3', '03A9F4', '00BCD4', '009688',
+        '4CAF50', '8BC34A', 'CDDC39', 'FFEB3B', 'FFC107', 'FF9800', 'FF5722', '795548', '9E9E9E', '607D8B'];
+
+        return Arr::random($colors);
+    }
+
+    /**
+     * Grabs an app setting using the given key.
+     * @param key The key to search by.
+     * @return Setting The desired setting, or `null` if it can't be found.
+     */
+    public static function settings($key)
+    {
+        return Setting::find($key)->value;
+    }
 }

44e69a9284647f598a2400321dd657cd89a33348
Curtis Upshall <curtisupshall@uvic.ca>
2020-04-22 14:51:19 -0700

    SPOT-197: touch Modals/ContextMenu

---
+++ frontend/src/components/Modals/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {
+    Menu
+} from '@material-ui/core'
+
+interface IContextMenuProps {
+
+}
+
+const ContextMenu = (props: IContextMenuProps) => {
+    return (
+        <Menu className='context-menu' />
+    )
+}
+
+export default ContextMenu

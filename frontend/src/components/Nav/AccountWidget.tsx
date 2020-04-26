a2417755ca7ce54fae92b4c37bbb490f36380125
Curtis Upshall <curtisupshall@uvic.ca>
2020-04-26 12:30:34 -0700

    SPOT-197: Add color to AccountWidget

--- frontend/src/components/Nav/AccountWidget.tsx
+++ frontend/src/components/Nav/AccountWidget.tsx
@@ -87,7 +87,9 @@ class AccountWidget extends React.Component<IAccountWidgetProps & IReduxProps, I
         return (
             <div className='account-widget'>
                 <NavItem title='My Account' onClick={this.handleClickOpen} orientation={this.props.orientation}>
-                    <Avatar className='account-widget__avatar' >{user ? user.avatar.initials : undefined}</Avatar>
+                    <Avatar className='account-widget__avatar' style={user ? {background: `#${user.avatar.color}`} : undefined}>
+                        {user ? user.avatar.initials : undefined}
+                    </Avatar>
                 </NavItem>
                 {user && (
                     <Menu

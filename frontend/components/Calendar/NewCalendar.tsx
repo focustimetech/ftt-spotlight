bdf482c49b60f57b6ecbb25e92f62d97fc0001e8
Curtis Upshall <curtisupshall@gmail.com>
2020-05-04 02:05:26 -0700

    Calendar date picker

--- frontend/components/Calendar/NewCalendar.tsx
+++ frontend/components/Calendar/NewCalendar.tsx
@@ -1,3 +1,4 @@
+import DateFnsUtils from '@date-io/date-fns'
 import moment from 'moment'
 import React from 'react'
 
@@ -7,6 +8,7 @@ import {
     endOfWeek,
     isToday,
     startOfWeek,
+    
     subDays,
     subWeeks,
     getMinutes
@@ -19,6 +21,7 @@ import {
     IconButton,
     Typography
 } from '@material-ui/core'
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
 
 import { getDaysOfWeek, getHoursOfDay, getTimeRangeLabels } from '../../utils/date'
 
@@ -49,13 +52,15 @@ interface ICalendarProps {
     weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6
 }
 
-interface IState {
+interface ICalendarState {
     currentDate: Date
+    pickerRef: EventTarget & HTMLButtonElement
 }
 
-class Calendar extends React.Component<ICalendarProps, IState> {
-    state: IState = {
-        currentDate: new Date()
+class Calendar extends React.Component<ICalendarProps, ICalendarState> {
+    state: ICalendarState = {
+        currentDate: new Date(),
+        pickerRef: null
     }
 
     handleSetToday = () => {
@@ -63,15 +68,27 @@ class Calendar extends React.Component<ICalendarProps, IState> {
     }
 
     handleNext = () => {
-        this.setState((state: IState) => ({
+        this.setState((state: ICalendarState) => ({
             currentDate: startOfWeek(addDays(endOfWeek(state.currentDate), 1))
         }))
     }
 
     handlePrevious = () => {
-        this.setState((state: IState) => ({
+        this.setState((state: ICalendarState) => ({
             currentDate: startOfWeek(subDays(startOfWeek(state.currentDate), 1))
         }))
+    }
+
+    handleOpenPicker = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        this.setState({ pickerRef: event.currentTarget })
+    }
+
+    handleClosePicker = () => {
+        this.setState({ pickerRef: null })
+    }
+
+    handleSelectDate = (date: Date) => {
+        this.setState({ currentDate: date, pickerRef: null })
     }
 
     render() {
@@ -81,6 +98,7 @@ class Calendar extends React.Component<ICalendarProps, IState> {
         const daysOfWeek: string[] = getDaysOfWeek(weekStartsOn)
         const hoursOfDay = getHoursOfDay()
         const calendarDayKeys: string[] = []
+        const pickerOpen: boolean = Boolean(this.state.pickerRef)
 
         return (
             <div className='new-calendar'>
@@ -89,6 +107,21 @@ class Calendar extends React.Component<ICalendarProps, IState> {
                     <IconButton onClick={() => this.handlePrevious()}><Icon>chevron_left</Icon></IconButton>
                     <IconButton onClick={() => this.handleNext()}><Icon>chevron_right</Icon></IconButton>
                     <CalendarMonthLabel date={weekStart} />
+                    <div>
+                        <IconButton onClick={this.handleOpenPicker}><Icon>today</Icon></IconButton>
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <DatePicker
+                                value={this.state.currentDate}
+                                onChange={this.handleSelectDate}
+                                onClose={this.handleClosePicker}
+                                disableToolbar
+                                variant='inline'
+                                open={pickerOpen}
+                                TextFieldComponent={() => null}
+                                PopoverProps={{anchorEl: this.state.pickerRef }}
+                            />
+                        </MuiPickersUtilsProvider>
+                    </div>
                 </Flexbox>
                 <div className='new-calendar__body'>
                     <div className='new-calendar__date-labels'>

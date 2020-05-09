aadd0e918e226b62c7c9fbe841217a1e7348c275
Curtis Upshall <curtisupshall@gmail.com>
2020-05-09 11:43:21 -0700

    Turn ColorPicker into Select element

--- frontend/components/Form/ColorPicker.tsx
+++ frontend/components/Form/ColorPicker.tsx
@@ -1,14 +1,21 @@
+import classNames from 'classnames'
 import React from 'react'
 
-import { Icon, Menu, Tooltip } from '@material-ui/core'
+import {
+    FormControl,
+    Icon,
+    InputLabel,
+    MenuItem,
+    Select,
+    Tooltip
+} from '@material-ui/core'
+import { FormControlProps } from '@material-ui/core/FormControl'
 
 import { COLORS } from '../../theme'
 
-interface IColorPickerProps {
-    anchorEl: Element
+interface IColorPickerProps extends Pick<FormControlProps, 'variant' | 'margin'> {
     value: string
     onChange: (hex: string) => void
-    onClose: () => void
 }
 
 export const randomColor = (): string => {
@@ -16,26 +23,34 @@ export const randomColor = (): string => {
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
-
 class ColorPicker extends React.Component<IColorPickerProps> {
     render() {
-        const open: boolean = Boolean(this.props.anchorEl)
+        const selectValue = this.props.value || '#DDD' // Used as a fallback
         const colors: string[] = Object.keys(COLORS)
 
         return (
-            <Menu className='color-picker' open={open} anchorEl={this.props.anchorEl} onClose={this.props.onClose}>
-                {colors.map((hex: string) => (
-                    <Tooltip title={COLORS[hex]} placement='bottom-start'>
-                        <a onClick={() => this.props.onChange(hex)}>
-                            <li className='color-picker__swatch' style={{ background: hex }}>
+            <FormControl className='color-picker' variant={this.props.variant} margin={this.props.margin}>
+                <InputLabel id='color-picker-label'>Color</InputLabel>
+                <Select
+                    value={selectValue}
+                    labelId='color-picker-label'
+                    MenuProps={{ className: 'color-picker__list', disablePortal: true }}
+                    label='Color'
+                    renderValue={(value: any) => (
+                        <div className={classNames('color-picker__swatch', '--select-value')} style={{ background: this.props.value }} />
+                    )}
+                >
+                    {colors.map((hex: string) => (
+                        <Tooltip title={COLORS[hex]} placement='bottom-start'>
+                            <MenuItem button={false} className='color-picker__swatch' style={{ background: hex }} value={hex} onClick={() => this.props.onChange(hex)}>
                                 {this.props.value === hex && (
                                     <Icon>check</Icon>
                                 )}
-                            </li>
-                        </a>
-                    </Tooltip>
-                ))}
-            </Menu>
+                            </MenuItem>
+                        </Tooltip>
+                    ))}
+                </Select>
+            </FormControl>
         )
     }
 }

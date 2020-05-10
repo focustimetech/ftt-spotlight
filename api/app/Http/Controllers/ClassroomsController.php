7ed521204c056b9d85179fd8ca47b01aa67aa611
Curtis Upshall <curtisupshall@gmail.com>
2020-05-10 08:38:20 -0700

    Rename ClassroomsController class name

--- api/app/Http/Controllers/ClassroomsController.php
+++ api/app/Http/Controllers/ClassroomsController.php
@@ -6,7 +6,7 @@ use App\Classroom;
 use App\Http\Resources\Classroom as ClassroomResource;
 use Illuminate\Http\Request;
 
-class ClassroomController extends Controller
+class ClassroomsController extends Controller
 {
     public function index(Request $request)
     {

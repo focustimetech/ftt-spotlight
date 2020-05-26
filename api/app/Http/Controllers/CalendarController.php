55ec5ac0cfa46f5cbc5df5ea3efecd71c8afd7db
Curtis Upshall <curtisupshall@gmail.com>
2020-05-25 17:57:02 -0700

    add ledger entries to CalendarController

--- api/app/Http/Controllers/CalendarController.php
+++ api/app/Http/Controllers/CalendarController.php
@@ -12,31 +12,36 @@ use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
-    private function getDateRangeFromTime($time)
+    private function getDateRangeFromTime($startTime)
     {
         return [
-            date('Y-m-d', strtotime('monday', $time)),
-            date('Y-m-d', strtotime('sunday', $time))
+            date('Y-m-d', $startTime),
+            date('Y-m-d', strtotime('+6 days', $startTime))
         ];
     }
 
     public function teacherCalendar($id, $date = null)
     {
         $time = $date ? strtotime($date) : time();
-        $dateRange = $this->getDateRangeFromTime($time);
         $startTime = strtotime('previous monday', $time);
+        $dateRange = $this->getDateRangeFromTime($startTime);
 
         $teacher = auth()->user()->account();
         $topicIds = $teacher->topics()->get()->pluck('id');
+        $ledgerEntries = $teacher->ledgerEntries()->whereBetween('date', $dateRange)->get();
 
         $blocks = Block::where('begins_on', '<=', date('Y-m-d H:i:s', $time))
             ->get()
             ->mapToGroups(function ($block, $key) use ($startTime, $topicIds) {
                 $date = date('Y-m-d', strtotime('+' . ($block->week_day - 1) . ' days', $startTime));
                 $context = [];
-
-                $context['location'] = new ClassroomResource(Classroom::all()->random());
-                $context['topic'] = $block->topics($date)->get()->whereIn('id', $topicIds)->first();
+                
+                $topic = $block->topics($date)->get()->whereIn('id', $topicIds)->first();
+                if ($topic) {
+                    $context['topic'] = $topic;
+                    $context['location'] = new ClassroomResource($topic->classroom()->first());
+                }
+               
                 return [$date => (new BlockResource($block))->context($context)];
             });
         

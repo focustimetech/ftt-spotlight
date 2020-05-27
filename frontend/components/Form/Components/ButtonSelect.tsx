58e06e6d4a66b7327e44ab8e308c93ecfa2f5845
Curtis Upshall <curtisupshall@gmail.com>
2020-05-26 19:39:47 -0700

    Add ButtonSelect-specific style

--- frontend/components/Form/Components/ButtonSelect.tsx
+++ frontend/components/Form/Components/ButtonSelect.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React from 'react'
 
 import Button, { ButtonProps } from '@material-ui/core/Button'
@@ -9,9 +10,13 @@ interface IButtonSelectProps extends ButtonProps {
 }
 
 const ButtonSelect = (props: IButtonSelectProps) => {
-    const { children, open, endIcon, ...rest } = props
+    const { children, className, open, endIcon, ...rest } = props
     return (
-        <Button {...rest} endIcon={endIcon || open ? <ArrowDropUp /> : <ArrowDropDown />}>
+        <Button
+            className={classNames(className, 'button-select')}
+            endIcon={endIcon || open ? <ArrowDropUp className='button-select__icon'/> : <ArrowDropDown className='button-select__icon'/>}
+            {...rest}
+        >
             {children}
         </Button>
     )

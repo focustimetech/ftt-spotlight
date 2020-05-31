55b0014d1b979b39c9180b7634233718250285d0
Curtis Upshall <curtisupshall@gmail.com>
2020-05-31 16:53:12 -0700

    Use _error page

--- frontend/pages/_error.tsx
+++ frontend/pages/_error.tsx
@@ -1,19 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
 import { NextPageContext } from 'next'
 
+import {
+    Typography
+} from '@material-ui/core'
+
 interface IErrorPageProps {
     statusCode: number
+    withNavigation?: boolean
 }
 
 class ErrorPage extends React.Component<IErrorPageProps> {
-    static getInitialProps = (context: NextPageContext): Partial<IErrorPageProps> => {
+    static getServersideProps = (context: NextPageContext): Partial<IErrorPageProps> => {
         const { res, err } = context
         const statusCode: number = res ? res.statusCode : err ? err.statusCode : 404
         return { statusCode }
     }
 
     render() {
-        return <div>Oh no! An error {this.props.statusCode} occured.</div>
+        return (
+            <div>
+                <Typography variant='h3'>_error.tsx: {this.props.statusCode};</Typography>
+                {this.props.withNavigation !== true && (
+                    <Link href='/'><a>Go to Home Page</a></Link>
+                )}
+            </div>
+        )
     }
 }
 

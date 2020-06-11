d5c696a406ce71ba921879cb09e07a7b88d64223
Curtis Upshall <curtisupshall@gmail.com>
2020-06-11 10:07:43 -0700

    Create type defs for ledger buffer

--- frontend/types/checkin.d.ts
+++ frontend/types/checkin.d.ts
@@ -1,44 +1,18 @@
-import { IFullDate, IBlock, ILedgerEntry, ISchedulePlan } from './calendar'
-import { IStudent } from './student';
-import { ISelectChip } from '../components/ChipSelect';
+import { IBlock } from './calendar'
+import { IStudent } from './auth';
 
-/*
-export interface CheckInStatusBlock {
-    block: IBlock
-    ledger_entries: ILedgerEntry[]
-    planned: ISchedulePlan[]
-}
-
-export interface CheckInStatus {
-    blocks: CheckInStatusBlock[]
-    date: IFullDate
-    next: string
-    previous: string
-    today: string
-}
-
-export interface ICheckInResponse {
-    success: ILedgerEntry[]
-    timestamp_string: string
-    errors?: string[]
-}
-
-export interface ICheckInError {
-    timestamp_string: string
-    errors: string[]
-}
+type CheckInChipStatus =
+    | 'queued'
+    | 'succeeded'
+    | 'failed'
 
 export interface ICheckInChip {
-    timestamp: string
-    value: string | number
+    value: string
+    timestamp: Date
+    status: CheckInChipStatus
 }
 
-export interface ICheckInRequest {
-    date?: string
-    type: 'student_number' | 'student_id'
-    chips: ICheckInChip[]
-}
-*/
+export type CheckInBuffer = Record<string, ICheckInChip>
 
 export interface ILedgerEntry {
     

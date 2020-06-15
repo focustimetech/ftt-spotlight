c206eff709a70f5f2fa341494a890242e08874e9
Curtis Upshall <curtisupshall@gmail.com>
2020-06-14 20:47:39 -0700

    Change purpose of Air check-in requests

--- api/database/migrations/2020_03_29_110807_create_air_requests_table.php
+++ api/database/migrations/2020_03_29_110807_create_air_requests_table.php
@@ -18,15 +18,11 @@ class CreateAirRequestsTable extends Migration
          */
         Schema::create('air_requests', function (Blueprint $table) {
             $table->id();
-            $table->string('memo')      // Optional memo on the purpose of the Air request
-                ->nullable()
-                ->default(null);
-            $table->foreignId('plan_id');    // The associated plan
-            $table->timestamps();       // Timestamp of when it was sent
+            $table->foreignId('student_id');
+            $table->foreignId('air_code_id');
             // Foreign keys
-            $table->foreign('plan_id')->references('id')->on('plans');
-            // Unique keys
-            $table->unique(['plan_id']);
+            $table->foreign('student_id')->referenes('id')->on('students');
+            $table->foreign('air_code_id')->referenes('id')->on('air_codes');
         });
     }
 

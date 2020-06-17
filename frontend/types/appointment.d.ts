3d648639847fbe38f9512a3184f98742284e16d3
Curtis Upshall <curtisupshall@gmail.com>
2020-06-17 15:17:04 -0700

    Create type defs for Appointments

--- frontend/types/appointment.d.ts
+++ frontend/types/appointment.d.ts
@@ -1,3 +1,17 @@
 export interface IAppointment {
+    id: number
     memo: string
+    date: string
+    teacherId: number
+    studentId: number
+    blockId: number
+    classroomId: number
+}
+
+export interface INewAppointment {
+    memo: string
+    date: string
+    blockId: number
+    classroomId: number
+    studentIds: number[]
 }

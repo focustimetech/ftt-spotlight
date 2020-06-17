4dd05f4c35b3785791f324c8116ca8dbf4ce2e3a
Curtis Upshall <curtisupshall@gmail.com>
2020-06-17 16:36:02 -0700

    Add jsdoc to pluralize util

--- frontend/utils/pluralize.ts
+++ frontend/utils/pluralize.ts
@@ -1,5 +1,12 @@
-const pluralize = (word: string, amount: number, singular: string = '', plural: string = 's') => {
-    return `${word}${amount === 1 ? singular : plural}`
+/**
+ * Pluralizes a noun.
+ * @param {string} [word] The noun being pluralized.
+ * @param {number} [amount] The amount.
+ * @param {string} [suffixSingular = ] The suffix given if the word is singular
+ * @param {string} [suffixPlural = s] The suffix given if the word is plural
+ */
+const pluralize = (word: string, amount: number, suffixSingular: string = '', suffixPlural: string = 's') => {
+    return `${word}${amount === 1 ? suffixSingular : suffixPlural}`
 }
 
 export default pluralize

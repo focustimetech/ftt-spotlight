1ae57fe7227a052ea8d3bf68a6322072db1ed6ef
Curtis Upshall <curtisupshall@gmail.com>
2020-06-17 15:07:20 -0700

    Add Appointments to cal context; Protect some context keys from students

--- api/app/Http/Controllers/CalendarController.php
+++ api/app/Http/Controllers/CalendarController.php
@@ -26,18 +26,20 @@ class CalendarController extends Controller
 
     public function teacherCalendar($id, $date = null)
     {
+        $user = auth()->user()->account();
         $time = $date ? strtotime($date) : time();
         error_log("\$time = ". date('Y-m-d H:i:s', $time));
         $startTime = strtotime('last monday', $time);
         $dateRange = $this->getDateRangeFromTime($startTime);
 
-        $teacher = auth()->user()->account();
+        $teacher = Teacher::findOrFail($id);
         $topicIds = $teacher->topics()->get()->pluck('id');
+        $appointments = $teacher->appointments()->whereBetween('date', $dateRange)->get();
         $ledgerEntries = $teacher->ledgerEntries()->whereBetween('date', $dateRange)->get();
 
         $blocks = Block::where('begins_on', '<=', date('Y-m-d H:i:s', $time))
             ->get()
-            ->mapToGroups(function ($block, $key) use ($startTime, $topicIds, $ledgerEntries, $teacher) {
+            ->mapToGroups(function ($block, $key) use ($startTime, $topicIds, $appointments, $ledgerEntries, $teacher) {
                 $date = date('Y-m-d', strtotime('+' . ($block->week_day - 1) . ' days', $startTime));
                 $blockId = $block->id;
                 $context = [];
@@ -47,19 +49,31 @@ class CalendarController extends Controller
                     $context['topic'] = $topic;
                     $context['location'] = new ClassroomResource($topic->classroom()->first());
                 }
-                
-                $airCode = AirCode::where('teacher_id', $teacher->id)
-                    ->where('block_id', $blockId)
-                    ->where('date', $date)
-                    ->where('expires_at', '<', date('Y-m-d H:i:s'))
-                    ->first();
-                if ($airCode) {
-                    $context['airCheckIn'] = new AirCodeResource($airCode);
-                }
 
-                $blockLedgerEntries = $ledgerEntries->where('date', $date)->where('block_id', $blockId);
-                if ($blockLedgerEntries) {
-                    $context['ledgerEntries'] = $blockLedgerEntries;
+                // Staff-only context items
+                if (in_array($user->account_type, ['staff', 'teacher', 'sysadmin'])) {
+                    $airCode = AirCode::where('teacher_id', $teacher->id)
+                        ->where('block_id', $blockId)
+                        ->where('date', $date)
+                        ->where('expires_at', '<', date('Y-m-d H:i:s'))
+                        ->first();
+                    if ($airCode) {
+                        $context['airCheckIn'] = new AirCodeResource($airCode);
+                    }
+
+                    $blockLedgerEntries = $ledgerEntries
+                        ->where('date', $date)
+                        ->where('block_id', $blockId);
+                    if ($blockLedgerEntries && count($blockLedgerEntries) > 0) {
+                        $context['ledgerEntries'] = $blockLedgerEntries;
+                    }
+
+                    $blockAppointments = $appointments
+                        ->where('date', $date)
+                        ->where('block_id', $blockId);
+                    if ($blockAppointments && count($blockAppointments) > 0) {
+                        $context['appointments'] = $blockAppointments;
+                    }
                 }
 
                 return [$date => (new BlockResource($block))->context($context)];

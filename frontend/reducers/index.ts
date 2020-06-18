ca172cfdad7832bba595c00a1e1a1eee9ce512df
Curtis Upshall <curtisupshall@gmail.com>
2020-06-17 17:11:22 -0700

    Remove unused reducers

--- frontend/reducers/index.ts
+++ frontend/reducers/index.ts
@@ -10,14 +10,9 @@ import { notificationReducer } from './notificationReducer'
 import { reportReducer } from './reportReducer'
 import { settingsReducer } from './settingsReducer'
 import { snackbarReducer } from './snackbarReducer'
-import { staffProfileReducer } from './staffProfileReducer'
 import { staffReducer } from './staffReducer'
-import { staffScheduleReducer } from './staffScheduleReducer'
-import { staffTopicsReducer } from './staffTopicsReducer'
 import { starReducer } from './starReducer'
-import { studentProfileReducer} from './studentProfileReducer'
 import { studentReducer } from './studentReducer'
-import { studentScheduleReducer } from './studentScheduleReducer'
 import { topicReducer } from './topicReducer'
 import { usersReducer } from './usersReducer'
 
@@ -32,14 +27,9 @@ export const rootReducer = combineReducers({
     settings: settingsReducer,
     snackbars: snackbarReducer,
     staff: staffReducer,
-    staffProfile: staffProfileReducer,
     starred: starReducer,
     students: studentReducer,
-    staffSchedule: staffScheduleReducer,
-    studentProfile: studentProfileReducer,
-    studentSchedule: studentScheduleReducer,
     reports: reportReducer,
     topics: topicReducer,
-    staffTopics: staffTopicsReducer,
     users: usersReducer
 })

d56a02e744dbb7bfd5461f1ddb2384e561fe6918
Curtis Upshall <curtisupshall@gmail.com>
2020-06-19 16:49:21 -0700

    Load style on the server

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import Document, {
     DocumentContext,
     DocumentInitialProps,
@@ -7,25 +9,39 @@ import Document, {
     NextScript
 } from 'next/document'
 
+import { ServerStyleSheets } from '@material-ui/core/styles'
+
 class SpotlightDocument extends Document {
     static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+        const sheets = new ServerStyleSheets()
+        const originalRenderPage = context.renderPage
+
+        context.renderPage = () => {
+            return originalRenderPage({
+                enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
+            })
+        }
+
         const initialProps = await Document.getInitialProps(context)
-        return initialProps
+        return {
+            ...initialProps,
+            styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()]
+        }
     }
 
     render() {
         return (
         <Html>
             <Head>
-            <meta charSet='UTF-8' />
-            <meta name='format-detection' content='telephone=no' />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
-            <meta name='msapplication-TileColor' content='#034670' />
-            <meta name='theme-color' content='#034670' />
-            <meta name='description' content='Start using powerful tools that let your self-directed study blocks succeed.' />
-            <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'></link>
-            <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' />
-            <link rel='shortcut icon' href='/images/favicon.ico' />
+                <meta charSet='UTF-8' />
+                <meta name='format-detection' content='telephone=no' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+                <meta name='msapplication-TileColor' content='#034670' />
+                <meta name='theme-color' content='#034670' />
+                <meta name='description' content='Start using powerful tools that let your self-directed study blocks succeed.' />
+                <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'></link>
+                <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' />
+                <link rel='shortcut icon' href='/images/favicon.ico' />
             </Head>
             <body>
                 <Main />

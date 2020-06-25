59617ece8598dc062ff1c2dca9f22522b9d7fb12
Curtis Upshall <curtisupshall@gmail.com>
2020-06-25 15:43:42 -0700

    Create Survey type defs

---
+++ frontend/src/types/survey.d.ts
@@ -0,0 +1,27 @@
+export interface ISurvey {
+    id: number
+    name: string
+    description: string
+    questions: ISurveyQuestion[]
+}
+
+export type INewSurvey = Omit<ISurvey, 'id'>
+
+export type ISurveyQuestion = ISurveyTextQuestion | ISurveyOptionQuestion & {
+    prompt: string
+    required: boolean
+}
+
+export type SurveyTextQuestionVariant = 'long' | 'short'
+
+export interface ISurveyTextQuestion {
+    variant: SurveyTextQuestionVariant
+}
+
+export type SurveyOptionQuestionVariant = 'radio' | 'checkbox' | 'select'
+
+export interface ISurveyOptionQuestion {
+    variant: SurveyOptionQuestionVariant
+    options: string[]
+    correctOptions?: number[]
+}

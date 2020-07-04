5228e515b4b634bdc725022d613a639fceb95590
Curtis Upshall <curtisupshall@uvic.ca>
2020-07-04 16:03:20 -0700

    touch DateBlockPicker

---
+++ frontend/src/components/Calendar/DateBlockPicker.tsx
@@ -0,0 +1,14 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import Flexbox from '../Layout/Flexbox'
+
+const DateBlockPicker = () => {
+    return (
+        <Flexbox justifyContent='space-between'>
+
+        </Flexbox>
+    )
+}
+
+export default DateBlockPicker

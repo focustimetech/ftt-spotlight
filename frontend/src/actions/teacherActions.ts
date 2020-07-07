6122eabda34b1e317f10c3f8f76ab9891fe255cb
Curtis Upshall <curtisupshall@gmail.com>
2020-07-07 16:07:42 -0700

    Undo deleting used action

--- frontend/src/actions/teacherActions.ts
+++ frontend/src/actions/teacherActions.ts
@@ -3,9 +3,13 @@ import API from '../utils/api'
 import { FETCH_TEACHERS } from './types'
 
 export const fetchTeachers = () => (dispatch: any) => {
-    return API.get<ITeacher>('/teachers')
+    return API.get<ITeacher[]>('/teachers')
         .then((res) => dispatch({
             type: FETCH_TEACHERS,
             payload: res.data
         }))
+}
+
+export const fetchTeacher = (teacherId: number) => {
+    return API.get<ITeacher>(`/teachers/${teacherId}`)
 }

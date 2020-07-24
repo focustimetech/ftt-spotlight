3284c8759e179a2e733fb184ee2a3e9c3ba178aa
Curtis Upshall <curtisupshall@gmail.com>
2020-07-24 14:19:50 -0700

    Change NavMenu into class component

--- frontend/src/components/Nav/NavMenu.tsx
+++ frontend/src/components/Nav/NavMenu.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { NextRouter, useRouter } from 'next/router'
+import { NextRouter, withRouter } from 'next/router'
 import React from 'react'
 
 import {
@@ -28,92 +28,116 @@ interface INavMenuProps {
     disableHiddenMenu?: boolean
 }
 
-const NavMenu = (props: INavMenuProps) => {
-    const [expanded, setExpanded] = React.useState(true)
-    const [showHidden, setShowHidden] = React.useState(false)
-    const router: NextRouter = useRouter()
-
-    React.useEffect(() => {
-        const menuState: any = localStorage.getItem(MENU_OPEN)
-        if (menuState === null) {
-            return
-        }
-        setExpanded(Boolean(Number(menuState)))
-    }, [])
-
-    const handleToggle = () => {
-        localStorage.setItem(MENU_OPEN, !expanded ? '1' : '0')
-        setExpanded(!expanded)
-    }
-
-    return (
-        <Paper>
-            <div className={classNames('nav-menu', { '--expanded': expanded })}>
-                <Fade in={expanded} unmountOnExit={false}>
-                    <div>
-                        <List className={classNames('nav-menu__list', '--expanded')}>
-                            <MenuItem className={classNames('nav-menu__toggle', 'nav-menu__item')} onClick={() => handleToggle()}>
-                                <ListItemIcon><Icon>chevron_left</Icon></ListItemIcon>
-                                <ListItemText>Close</ListItemText>
-                            </MenuItem>
-                            {props.menuItems.map((menuItem) => (
-                                <NavMenuItem
-                                    {...menuItem}
-                                    useListItem
-                                    active={router.asPath === menuItem.as}
-                                    key={menuItem.label}
-                                />
-                            ))}
-                            {props.hiddenMenuItems && (
-                                <>
-                                    <Collapse in={showHidden}>
-                                        <div>
-                                            <Divider />
-                                            {props.hiddenMenuItems.map((menuItem: INavMenuItem) => (
-                                                <NavMenuItem
-                                                    {...menuItem}
-                                                    useListItem
-                                                    active={router.asPath === menuItem.as}
-                                                    key={menuItem.label}
-                                                />
-                                            ))}
-                                        </div>
-                                    </Collapse>
-                                    <MenuItem
-                                        className='nav-menu__toggle'
-                                        onClick={() => setShowHidden(!showHidden)}
-                                        disabled={props.hiddenMenuItems.length === 0 || props.disableHiddenMenu}
-                                    >
-                                        <ListItemIcon><Icon>{showHidden ? 'expand_less' : 'expand_more'}</Icon></ListItemIcon>
-                                        <ListItemText>{showHidden ? 'Show less' : 'Show more'}</ListItemText>
-                                    </MenuItem>
-                                </>
-                            )}
-                        </List>
-                    </div>
-                </Fade>
-                <Fade in={!expanded} unmountOnExit={false}>
-                    <div>
-                        <List className='nav-menu__list'>
-                            <li>
-                                <IconButton onClick={() => handleToggle()}>
-                                    <Icon>chevron_right</Icon>
-                                </IconButton>
-                            </li>
-                            {props.menuItems.map((menuItem) => (
-                                <NavMenuItem
-                                    {...menuItem}
-                                    useListItem={false}
-                                    active={router.asPath === menuItem.as}
-                                    key={menuItem.label}
-                                />
-                            ))}
-                        </List>
-                    </div>
-                </Fade>
-            </div>
-        </Paper>
-    )
+interface INavMenuState {
+    expanded: boolean
+    showHidden: boolean
 }
 
-export default NavMenu
+interface IRouterProps {
+    router: NextRouter
+}
+
+class NavMenu extends React.Component<INavMenuProps & IRouterProps, INavMenuState> {
+    state: INavMenuState = {
+        expanded: false,
+        showHidden: false,
+    }
+
+    toggleExpanded = () => {
+        this.setState((state: INavMenuState) => {
+            const expanded = !state.expanded
+            localStorage.setItem(MENU_OPEN, expanded ? '1' : '0')
+            return { expanded }
+        })
+    }
+
+    toggleShowHidden = () => {
+        this.setState((state: INavMenuState) => ({
+            showHidden: !state.showHidden
+        }))
+    }
+
+    componentWillMount() {
+        if (typeof window === 'undefined') {
+            return
+        }
+        this.setState({ expanded: Boolean(Number(localStorage.getItem(MENU_OPEN))) })
+    }
+
+    render() {
+        console.log('NavMenu.state:', this.state)
+        const { expanded, showHidden } = this.state
+        const { menuItems, hiddenMenuItems, disableHiddenMenu, router } = this.props
+
+        return (
+            <Paper>
+                <div className={classNames('nav-menu', { '--expanded': expanded })}>
+                    <Fade in={expanded} unmountOnExit={false}>
+                        <div>
+                            <List className={classNames('nav-menu__list', '--expanded')}>
+                                <MenuItem className={classNames('nav-menu__toggle', 'nav-menu__item')} onClick={() => this.toggleExpanded()}>
+                                    <ListItemIcon><Icon>chevron_left</Icon></ListItemIcon>
+                                    <ListItemText>Close</ListItemText>
+                                </MenuItem>
+                                {menuItems.map((menuItem) => (
+                                    <NavMenuItem
+                                        {...menuItem}
+                                        useListItem
+                                        active={router.asPath === menuItem.as}
+                                        key={menuItem.label}
+                                    />
+                                ))}
+                                {hiddenMenuItems && (
+                                    <>
+                                        <Collapse in={showHidden}>
+                                            <div>
+                                                <Divider />
+                                                {hiddenMenuItems.map((menuItem: INavMenuItem) => (
+                                                    <NavMenuItem
+                                                        {...menuItem}
+                                                        useListItem
+                                                        active={router.asPath === menuItem.as}
+                                                        key={menuItem.label}
+                                                    />
+                                                ))}
+                                            </div>
+                                        </Collapse>
+                                        <MenuItem
+                                            className='nav-menu__toggle'
+                                            onClick={() => this.toggleShowHidden()}
+                                            disabled={hiddenMenuItems.length === 0 || disableHiddenMenu}
+                                        >
+                                            <ListItemIcon><Icon>{showHidden ? 'expand_less' : 'expand_more'}</Icon></ListItemIcon>
+                                            <ListItemText>{showHidden ? 'Show less' : 'Show more'}</ListItemText>
+                                        </MenuItem>
+                                    </>
+                                )}
+                            </List>
+                        </div>
+                    </Fade>
+                    <Fade in={!expanded} unmountOnExit={false}>
+                        <div>
+                            <List className='nav-menu__list'>
+                                <li>
+                                    <IconButton onClick={() => this.toggleExpanded()}>
+                                        <Icon>chevron_right</Icon>
+                                    </IconButton>
+                                </li>
+                                {menuItems.map((menuItem) => (
+                                    <NavMenuItem
+                                        {...menuItem}
+                                        useListItem={false}
+                                        active={router.asPath === menuItem.as}
+                                        key={menuItem.label}
+                                    />
+                                ))}
+                            </List>
+                        </div>
+                    </Fade>
+                </div>
+            </Paper>
+        )
+    }
+}
+
+export default withRouter(NavMenu)
